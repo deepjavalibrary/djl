e2441c68792d2e13913c6724a2bbfb962983d3f3
Qing <lanking520@live.com>
2019-05-14 19:10:57 -0700

    optimized Dataparser

--- example/src/main/java/com/amazon/ai/example/util/BertDataParser.java
+++ example/src/main/java/com/amazon/ai/example/util/BertDataParser.java
@@ -1,19 +1,11 @@
 package com.amazon.ai.example.util;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.annotations.SerializedName;
-
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -24,14 +16,8 @@ import java.util.Map;
  */
 public class BertDataParser {
 
-    private class VocabularyInfo {
-        @SerializedName("token_to_idx")
-        private Map<String, Integer> token2idx;
-        @SerializedName("idx_to_token")
-        private List<String> idx2token;
-    }
-
-    private VocabularyInfo vocab;
+    private Map<String, Integer> token2idx;
+    private List<String> idx2token;
 
     /**
      * Parse the Vocabulary to JSON files [PAD], [CLS], [SEP], [MASK], [UNK] are reserved tokens
@@ -41,7 +27,11 @@ public class BertDataParser {
      */
     public BertDataParser(String jsonFile) throws IOException {
         Gson gson = new Gson();
-        vocab = gson.fromJson(Files.newBufferedReader(new File(jsonFile).toPath()), VocabularyInfo.class);
+        VocabularyInfo vocab =
+                gson.fromJson(
+                        Files.newBufferedReader(new File(jsonFile).toPath()), VocabularyInfo.class);
+        token2idx = vocab.token2idx;
+        idx2token = vocab.idx2token;
     }
 
     /**
@@ -87,8 +77,8 @@ public class BertDataParser {
     }
 
     /**
-     * Form the token types List [0000...1111...000]
-     * All questions are 0 and Answers are 1
+     * Form the token types List [0000...1111...000] All questions are 0 and Answers are 1
+     *
      * @param question question tokens
      * @param answer answer tokens
      * @param seqLength sequence length
@@ -103,6 +93,7 @@ public class BertDataParser {
 
     /**
      * Form tokens with separation that can be used for BERT
+     *
      * @param question question tokens
      * @param answer answer tokens
      * @param seqLength sequence length
@@ -128,10 +119,10 @@ public class BertDataParser {
     public List<Integer> token2idx(List<String> tokens) {
         List<Integer> indexes = new ArrayList<>();
         for (String token : tokens) {
-            if (vocab.token2idx.containsKey(token)) {
-                indexes.add(vocab.token2idx.get(token));
+            if (token2idx.containsKey(token)) {
+                indexes.add(token2idx.get(token));
             } else {
-                indexes.add(vocab.token2idx.get("[UNK]"));
+                indexes.add(token2idx.get("[UNK]"));
             }
         }
         return indexes;
@@ -146,8 +137,16 @@ public class BertDataParser {
     public List<String> idx2token(List<Integer> indexes) {
         List<String> tokens = new ArrayList<>();
         for (int index : indexes) {
-            tokens.add(vocab.idx2token.get(index));
+            tokens.add(idx2token.get(index));
         }
         return tokens;
+    }
+
+    private class VocabularyInfo {
+        @SerializedName("token_to_idx")
+        private Map<String, Integer> token2idx;
+
+        @SerializedName("idx_to_token")
+        private List<String> idx2token;
     }
 }
