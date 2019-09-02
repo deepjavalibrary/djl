90d9fc8cb1467cc3310c67d9921c09ffaed5aa8a
Frank Liu <frankfliu2000@gmail.com>
2019-09-02 14:08:57 -0700

    Make repository download atomic.
    Change-Id: I1c2e1f5698900301d049819a0998442a0cdfe076

--- repository/src/main/java/software/amazon/ai/repository/AbstractRepository.java
+++ repository/src/main/java/software/amazon/ai/repository/AbstractRepository.java
@@ -18,9 +18,11 @@ import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Map;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.ZipInputStream;
+import software.amazon.ai.util.Utils;
 
 public abstract class AbstractRepository implements Repository {
 
@@ -58,64 +60,28 @@ public abstract class AbstractRepository implements Repository {
         URI resourceUri = artifact.getResourceUri();
         Path resourceDir = cacheDir.resolve(resourceUri.getPath());
         if (Files.exists(resourceDir)) {
+            // files have been downloaded already.
             return;
         }
 
-        // TODO: extract to temp directory first.
-        Files.createDirectories(resourceDir);
         Metadata metadata = artifact.getMetadata();
         URI baseUri = metadata.getRepositoryUri();
         Map<String, Artifact.Item> files = artifact.getFiles();
-        for (Map.Entry<String, Artifact.Item> entry : files.entrySet()) {
-            Artifact.Item item = entry.getValue();
-            URI fileUri = URI.create(item.getUri());
-            if (!fileUri.isAbsolute()) {
-                fileUri = getBaseUri().resolve(baseUri).resolve(fileUri);
-            }
 
-            // This is file is on remote, download it
-            String fileName = item.getName();
-            String extension = item.getExtension();
-            if ("dir".equals(item.getType())) {
-                Path dir;
-                if (!fileName.isEmpty()) {
-                    // honer the name set in metadata.json
-                    dir = resourceDir.resolve(fileName);
-                    Files.createDirectories(dir);
-                } else {
-                    dir = resourceDir;
-                }
-                if (!"zip".equals(extension)) {
-                    throw new UnsupportedOperationException(
-                            "File type is not supported: " + extension);
-                }
-                try (InputStream is = fileUri.toURL().openStream()) {
-                    ZipUtils.unzip(is, dir);
-                }
-                return;
-            }
-
-            try (InputStream is = fileUri.toURL().openStream()) {
-                Path file = resourceDir.resolve(fileName);
-                if ("zip".equals(extension)) {
-                    try (ZipInputStream zis = new ZipInputStream(is)) {
-                        zis.getNextEntry();
-                        Files.copy(zis, file);
-                    }
-                } else if ("gzip".equals(extension)) {
-                    try (GZIPInputStream zis = new GZIPInputStream(is)) {
-                        Files.copy(zis, file);
-                    }
-                } else if (extension.isEmpty()) {
-                    Files.copy(is, file);
-                } else {
-                    throw new UnsupportedOperationException(
-                            "File type is not supported: " + extension);
-                }
-            }
+        Path parentDir = resourceDir.toAbsolutePath().getParent();
+        if (parentDir == null) {
+            throw new AssertionError("Parent path should never be null: " + resourceDir.toString());
         }
-
-        // TODO: clean up obsoleted files
+        Files.createDirectories(parentDir);
+        Path tmp = Files.createTempDirectory(parentDir, resourceDir.toFile().getName());
+        try {
+            for (Artifact.Item item : files.values()) {
+                download(tmp, baseUri, item);
+            }
+            Files.move(tmp, resourceDir, StandardCopyOption.ATOMIC_MOVE);
+        } finally {
+            Utils.deleteQuietly(tmp);
+        }
     }
 
     @Override
@@ -136,5 +102,50 @@ public abstract class AbstractRepository implements Repository {
             throw new IOException("Failed initialize cache directory: " + dir.toString());
         }
         return dir;
+    }
+
+    private void download(Path tmp, URI baseUri, Artifact.Item item) throws IOException {
+        URI fileUri = URI.create(item.getUri());
+        if (!fileUri.isAbsolute()) {
+            fileUri = getBaseUri().resolve(baseUri).resolve(fileUri);
+        }
+
+        String fileName = item.getName();
+        String extension = item.getExtension();
+        if ("dir".equals(item.getType())) {
+            Path dir;
+            if (!fileName.isEmpty()) {
+                // honer the name set in metadata.json
+                dir = tmp.resolve(fileName);
+                Files.createDirectories(dir);
+            } else {
+                dir = tmp;
+            }
+            if (!"zip".equals(extension)) {
+                throw new IOException("File type is not supported: " + extension);
+            }
+            try (InputStream is = fileUri.toURL().openStream()) {
+                ZipUtils.unzip(is, dir);
+            }
+            return;
+        }
+
+        try (InputStream is = fileUri.toURL().openStream()) {
+            Path file = tmp.resolve(fileName);
+            if ("zip".equals(extension)) {
+                try (ZipInputStream zis = new ZipInputStream(is)) {
+                    zis.getNextEntry();
+                    Files.copy(zis, file);
+                }
+            } else if ("gzip".equals(extension)) {
+                try (GZIPInputStream zis = new GZIPInputStream(is)) {
+                    Files.copy(zis, file);
+                }
+            } else if (extension.isEmpty()) {
+                Files.copy(is, file);
+            } else {
+                throw new IOException("File type is not supported: " + extension);
+            }
+        }
     }
 }
--- repository/src/main/java/software/amazon/ai/repository/Repository.java
+++ repository/src/main/java/software/amazon/ai/repository/Repository.java
@@ -17,12 +17,9 @@ import com.google.gson.GsonBuilder;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.ZipInputStream;
 
 public interface Repository {
 
@@ -56,73 +53,7 @@ public interface Repository {
 
     InputStream openStream(Artifact.Item item, String path) throws IOException;
 
-    default void prepare(Artifact artifact) throws IOException {
-        Path cacheDir = getCacheDirectory();
-        URI resourceUri = artifact.getResourceUri();
-        Path resourceDir = cacheDir.resolve(resourceUri.getPath());
-        // files have been downloaded
-        if (Files.exists(resourceDir)) {
-            return;
-        }
-
-        // TODO: extract to temp directory first.
-        Files.createDirectories(resourceDir);
-        Metadata metadata = artifact.getMetadata();
-        URI baseUri = metadata.getRepositoryUri();
-        Map<String, Artifact.Item> files = artifact.getFiles();
-        for (Map.Entry<String, Artifact.Item> entry : files.entrySet()) {
-            Artifact.Item item = entry.getValue();
-            URI fileUri = URI.create(item.getUri());
-            if (!fileUri.isAbsolute()) {
-                fileUri = getBaseUri().resolve(baseUri).resolve(fileUri);
-            }
-
-            // This is file is on remote, download it
-            String fileName = item.getName();
-            String extension = item.getExtension();
-            if ("dir".equals(item.getType())) {
-                Path dir;
-                if (!fileName.isEmpty()) {
-                    // honer the name set in metadata.json
-                    dir = resourceDir.resolve(fileName);
-                    Files.createDirectories(dir);
-                } else {
-                    dir = resourceDir;
-                }
-                if (!"zip".equals(extension) && !"tgz".equals(extension)) {
-                    throw new UnsupportedOperationException(
-                            "File type is not supported: " + extension);
-                }
-                try (InputStream is = fileUri.toURL().openStream()) {
-                    if ("zip".equals(extension)) {
-                        ZipUtils.unzip(is, dir);
-                    }
-                }
-                return;
-            }
-
-            try (InputStream is = fileUri.toURL().openStream()) {
-                Path file = resourceDir.resolve(fileName);
-                if ("zip".equals(extension)) {
-                    try (ZipInputStream zis = new ZipInputStream(is)) {
-                        zis.getNextEntry();
-                        Files.copy(zis, file);
-                    }
-                } else if ("gzip".equals(extension)) {
-                    try (GZIPInputStream zis = new GZIPInputStream(is)) {
-                        Files.copy(zis, file);
-                    }
-                } else if (extension.isEmpty()) {
-                    Files.copy(is, file);
-                } else {
-                    throw new UnsupportedOperationException(
-                            "File type is not supported: " + extension);
-                }
-            }
-        }
-
-        // TODO: clean up obsoleted files
-    }
+    void prepare(Artifact artifact) throws IOException;
 
     Path getCacheDirectory() throws IOException;
 }
