7db1ab8f28ccf374c504ff281fd83a72a0255b03
Qing <qingla@amazon.com>
2019-11-01 15:14:08 -0700

    update benchmark download file
    Change-Id: Id1bbb293ac47e3224d1516bc345b1eb883617ac6

--- tools/scripts/benchmark.sh
+++ tools/scripts/benchmark.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 curl -o dogs.jpg https://raw.githubusercontent.com/awslabs/mxnet-model-server/master/docs/images/3dogs.jpg
-curl -o soccer.png https://github.com/dmlc/web-data/blob/master/gluoncv/pose/soccer.png?raw=true
+curl -o soccer.png https://raw.githubusercontent.com/dmlc/web-data/master/gluoncv/pose/soccer.png
 ./gradlew run -Dmain=ai.djl.examples.inference.ClassifyExample --args="-i dogs.jpg -c 1000 -r {'layers':'18','flavor':'v1'}" &> res18.log
 ./gradlew run -Dmain=ai.djl.examples.inference.ClassifyExample --args="-i dogs.jpg -c 1000 -r {'layers':'50','flavor':'v2'}" &> res50.log
 ./gradlew run -Dmain=ai.djl.examples.inference.ClassifyExample --args="-i dogs.jpg -c 1000 -r {'layers':'152','flavor':'v1d'}" &> res152.log
