c567b991648e9816a8529abffa63b71a5398942d
Frank Liu <frankfliu2000@gmail.com>
2019-11-04 09:20:17 -0800

    Refactor Mutli-threaded inference example.
    Change-Id: I1b2281216e8a42dcee1cef31da087918155b33bf

--- examples/src/main/java/ai/djl/examples/inference/MultithreadedInferenceExample.java
+++ examples/src/main/java/ai/djl/examples/inference/MultithreadedInferenceExample.java
@@ -12,7 +12,6 @@
  */
 package ai.djl.examples.inference;
 
-import ai.djl.Device;
 import ai.djl.ModelException;
 import ai.djl.examples.inference.util.AbstractInference;
 import ai.djl.examples.inference.util.Arguments;
@@ -21,6 +20,7 @@ import ai.djl.metric.Metrics;
 import ai.djl.modality.Classification;
 import ai.djl.modality.cv.util.BufferedImageUtils;
 import ai.djl.mxnet.zoo.MxModelZoo;
+import ai.djl.repository.zoo.ModelLoader;
 import ai.djl.repository.zoo.ZooModel;
 import ai.djl.translate.TranslateException;
 import java.awt.image.BufferedImage;
@@ -54,85 +54,45 @@ public class MultithreadedInferenceExample extends AbstractInference<Classificat
         Path imageFile = arguments.getImageFile();
         BufferedImage img = BufferedImageUtils.fromFile(imageFile);
 
-        // Device is not required, default device will be used by Model if not provided.
-        // Change to a specific device if needed.
-        Device device = Device.defaultDevice();
-
+        String modelName = arguments.getModelName();
+        if (modelName == null) {
+            modelName = "RESNET";
+        }
         Map<String, String> criteria = arguments.getCriteria();
         if (criteria == null) {
             criteria = new ConcurrentHashMap<>();
             criteria.put("layers", "18");
             criteria.put("flavor", "v1");
         }
-        ZooModel<BufferedImage, Classification> model =
-                MxModelZoo.RESNET.loadModel(criteria, device);
+        ModelLoader<BufferedImage, Classification> loader = MxModelZoo.getModelLoader(modelName);
+        ZooModel<BufferedImage, Classification> model = loader.loadModel(criteria);
 
+        int numOfThreads = Runtime.getRuntime().availableProcessors();
+        List<PredictorCallable> callables =
+                Collections.nCopies(
+                        numOfThreads, new PredictorCallable(model, img, metrics, iteration));
+
+        Classification classification = null;
+        ExecutorService executorService = Executors.newFixedThreadPool(numOfThreads);
         try {
-            return multiPredictorPredict(img, iteration, model, metrics);
+            List<Future<Classification>> futures = executorService.invokeAll(callables);
+            for (Future<Classification> future : futures) {
+                classification = future.get();
+            }
         } catch (InterruptedException | ExecutionException e) {
-            logger.error(e.getMessage());
-        }
-        return null;
-    }
-
-    public Classification singlePredictorPredict(
-            BufferedImage img,
-            int iterationPerThread,
-            ZooModel<BufferedImage, Classification> model,
-            Metrics metrics)
-            throws ExecutionException, InterruptedException {
-        int numOfThreads = Runtime.getRuntime().availableProcessors();
-        Predictor<BufferedImage, Classification> predictor = model.newPredictor();
-        List<PredictorCallable> callables =
-                Collections.nCopies(
-                        numOfThreads,
-                        new PredictorCallable(predictor, img, metrics, iterationPerThread));
-        ExecutorService executorService = Executors.newFixedThreadPool(numOfThreads);
-        List<Future<Classification>> futures = executorService.invokeAll(callables);
-        Classification classification = null;
-        for (Future<Classification> future : futures) {
-            classification = future.get();
+            logger.error("", e);
+        } finally {
+            executorService.shutdown();
         }
         return classification;
     }
 
-    public Classification multiPredictorPredict(
-            BufferedImage img,
-            int iterationPerThread,
-            ZooModel<BufferedImage, Classification> model,
-            Metrics metrics)
-            throws ExecutionException, InterruptedException {
-        int numOfThreads = Runtime.getRuntime().availableProcessors();
-        List<PredictorCallable> callables =
-                Collections.nCopies(
-                        numOfThreads,
-                        new PredictorCallable(model, img, metrics, iterationPerThread));
-        ExecutorService executorService = Executors.newFixedThreadPool(numOfThreads);
-        List<Future<Classification>> futures = executorService.invokeAll(callables);
-        Classification classification = null;
-        for (Future<Classification> future : futures) {
-            classification = future.get();
-        }
-        return classification;
-    }
-
-    static class PredictorCallable implements Callable<Classification> {
+    private static class PredictorCallable implements Callable<Classification> {
 
         private Predictor<BufferedImage, Classification> predictor;
         private BufferedImage img;
         private Metrics metrics;
         private int iteration;
-
-        public PredictorCallable(
-                Predictor<BufferedImage, Classification> predictor,
-                BufferedImage img,
-                Metrics metrics,
-                int iteration) {
-            this.predictor = predictor;
-            this.img = img;
-            this.metrics = metrics;
-            this.iteration = iteration;
-        }
 
         public PredictorCallable(
                 ZooModel<BufferedImage, Classification> model,
