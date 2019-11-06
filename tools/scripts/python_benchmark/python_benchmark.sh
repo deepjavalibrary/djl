8aa0abce42b16e4f5230883fb732b81196419b9a
Qing <qingla@amazon.com>
2019-11-06 09:57:23 -0800

    update the training script
    Change-Id: I07419cf51a4063d042076df65b19f35758cbb5c2

--- tools/scripts/python_benchmark/python_benchmark.sh
+++ tools/scripts/python_benchmark/python_benchmark.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-python training/trainCiFar10.py --epochs 10 --model resnet50_v1 --gpus 1 --mode hybrid --lr 0.001 --wd 0.001 --use_thumbnail
+python training/trainCiFar10.py --epochs 10 --model resnet50_v1 --mode hybrid --lr 0.001 --wd 0.001 --gpus 0 --use_thumbnail
 mv image-classification.log image-classification-symbolic.log
-python training/trainCiFar10.py --epochs 10 --model resnet50_v1 --gpus 1  --mode hybrid --lr 0.001 --wd 0.001 --use-pretrained --use_thumbnail
-mv image-classification.log image-classification-symbolic-pretrained.log
-python training/trainCiFar10.py --epochs 10 --model resnet50_v1 --gpus 1 --mode imperative --lr 0.001 --wd 0.001 --use_thumbnail
+python training/trainCiFar10.py --epochs 10 --model resnet50_v1 --mode imperative --lr 0.001 --wd 0.001 --gpus 0 --use_thumbnail
 mv image-classification.log image-classification-imperative.log
