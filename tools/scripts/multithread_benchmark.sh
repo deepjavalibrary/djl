b3f5f97ef8ed05c25193f6f71ab7369fc3c1637b
Jake Lee <leecheng@amazon.com>
2019-12-12 12:58:11 -0800

    Move benchmark script to deeplearning-benchmark
    Change-Id: I1d9ef49bd5510611657fdc80acead9bc3b35f21c

--- tools/scripts/multithread_benchmark.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-curl -O https://raw.githubusercontent.com/awslabs/mxnet-model-server/master/docs/images/3dogs.jpg
-export export MXNET_ENGINE_TYPE=NaiveEngine
-./gradlew run -Dmain=ai.djl.examples.inference.Benchmark -Dcollect-memory=true --args="-c 2000 -i 3dogs.jpg -r {'layers':'18','flavor':'v1'}" &> res18.log
-./gradlew run -Dmain=ai.djl.examples.inference.Benchmark -Dcollect-memory=true --args="-c 1600 -i 3dogs.jpg -r {'layers':'18','flavor':'v1'}" &> res18.log
-./gradlew run -Dmain=ai.djl.examples.inference.MultithreadedBenchmark -Dcollect-memory=true --args="-c 100 -i 3dogs.jpg -r {'layers':'18','flavor':'v1'}" &> multithread.log
-./gradlew run -Dmain=ai.djl.examples.inference.MultithreadedBenchmark -Dcollect-memory=true -DMXNET_THREAD_SAFE_INFERENCE=true --args="-c 100 -i 3dogs.jpg -r {'layers':'18','flavor':'v1'}" &> multithread_threadsafe.log
-./gradlew run -Dmain=ai.djl.examples.inference.MultithreadedBenchmark -Dcollect-memory=true --args="-c 100 -i 3dogs.jpg -m -r {'layers':'50','flavor':'v1'}" &> multithread_imp.log
-
--- tools/scripts/train_benchmark.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-./gradlew run -Dmain=ai.djl.examples.training.transferlearning.TrainResnetWithCifar10 --args="-b 32 -e 10 -o logs/" &> res50Cifar10Imp.log
-./gradlew run -Dmain=ai.djl.examples.training.transferlearning.TrainResnetWithCifar10 --args="-b 32 -e 10 -o logs/ -s" &> res50Cifar10Sym.log
-./gradlew run -Dmain=ai.djl.examples.training.transferlearning.TrainResnetWithCifar10 --args="-b 32 -e 10 -o logs/ -s -p" &> res50Cifar10SymPretrain.log
