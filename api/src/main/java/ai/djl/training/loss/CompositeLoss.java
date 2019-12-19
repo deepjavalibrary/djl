7201517e0093b594a86a65cb4264a0bc2f8b40a0
Zach Kimberg <kimbergz@amazon.com>
2019-12-18 17:50:48 -0800

    Add indices to composite loss
    Change-Id: I5582b2a53463ee3ae678b3ef04ac6a06de0c7b82

--- api/src/main/java/ai/djl/training/loss/CompositeLoss.java
+++ api/src/main/java/ai/djl/training/loss/CompositeLoss.java
@@ -15,7 +15,8 @@ package ai.djl.training.loss;
 import ai.djl.ndarray.NDArray;
 import ai.djl.ndarray.NDArrays;
 import ai.djl.ndarray.NDList;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * {@code CompositeLoss} is an implementation of the {@link Loss} abstract class that can combine
@@ -23,40 +24,91 @@ import java.util.Arrays;
  */
 public class CompositeLoss extends Loss {
 
-    private Loss[] components;
+    private List<Loss> components;
+    private List<Integer> indices;
 
     /**
-     * Creates a new instance of {@code CompositeLoss} that can combine the given {@link Loss}
+     * Creates a new empty instance of {@code CompositeLoss} that can combine the given {@link Loss}
      * components.
-     *
-     * @param components the {@code Loss} objects that form the composite loss
      */
-    public CompositeLoss(Loss... components) {
+    public CompositeLoss() {
         super("CompositeLoss");
-        this.components = components;
+        components = new ArrayList<>();
+        indices = new ArrayList<>();
+    }
+
+    /**
+     * Adds a Loss that applies to all labels and predictions to this composite loss.
+     *
+     * @param loss the loss to add
+     * @return this composite loss
+     */
+    public Loss addLoss(Loss loss) {
+        components.add(loss);
+        indices.add(null);
+        return this;
+    }
+
+    /**
+     * Adds a Loss that applies to a single index of the label and predictions to this composite
+     * loss.
+     *
+     * @param loss the loss to add
+     * @param index the index in the label and predictions NDLists this loss applies to
+     * @return this composite loss
+     */
+    public Loss addLoss(Loss loss, int index) {
+        components.add(loss);
+        indices.add(index);
+        return this;
     }
 
     /** {@inheritDoc} */
     @Override
-    public NDArray getLoss(NDList label, NDList prediction) {
-        return NDArrays.add(
-                Arrays.stream(components)
-                        .map(component -> component.getLoss(label, prediction))
-                        .toArray(NDArray[]::new));
+    public NDArray getLoss(NDList labels, NDList predictions) {
+        NDArray[] lossComponents = new NDArray[components.size()];
+        for (int i = 0; i < components.size(); i++) {
+            if (indices.get(i) != null) {
+                int index = indices.get(i);
+                lossComponents[i] =
+                        components
+                                .get(i)
+                                .getLoss(
+                                        new NDList(labels.get(index)),
+                                        new NDList(predictions.get(index)));
+            } else {
+                lossComponents[i] = components.get(i).getLoss(labels, predictions);
+            }
+        }
+        return NDArrays.add(lossComponents);
     }
 
     /** {@inheritDoc} */
     @Override
     public Loss duplicate() {
-        return new CompositeLoss(
-                Arrays.stream(components).map(Loss::duplicate).toArray(Loss[]::new));
+        CompositeLoss dup = new CompositeLoss();
+        for (int i = 0; i < components.size(); i++) {
+            if (indices.get(i) != null) {
+                dup.addLoss(components.get(i).duplicate(), indices.get(i));
+            } else {
+                dup.addLoss(components.get(i).duplicate());
+            }
+        }
+        return dup;
     }
 
     /** {@inheritDoc} */
     @Override
     public void update(NDList labels, NDList predictions) {
-        for (Loss component : components) {
-            component.update(labels, predictions);
+        for (int i = 0; i < components.size(); i++) {
+            if (indices.get(i) != null) {
+                int index = indices.get(i);
+                components
+                        .get(i)
+                        .update(new NDList(labels.get(index)), new NDList(predictions.get(index)));
+            } else {
+                components.get(i).update(labels, predictions);
+            }
         }
     }
 
@@ -71,6 +123,6 @@ public class CompositeLoss extends Loss {
     /** {@inheritDoc} */
     @Override
     public float getValue() {
-        return (float) Arrays.stream(components).mapToDouble(Loss::getValue).sum();
+        return (float) components.stream().mapToDouble(Loss::getValue).sum();
     }
 }
