9dee22b1d98d3490c9a9c9653f15d66f321a7c3b
Frank Liu <frankfliu2000@gmail.com>
2020-01-31 13:30:29 -0800

    Update pytorch native build script.
    Change-Id: I5393a2f84d8ef377be820aaafc2d115bfa5bf287

--- pytorch/native/README.md
+++ pytorch/native/README.md
@@ -1,7 +1,7 @@
 # Step to build this project
-- Download the libtorch from [link](https://download.pytorch.org/libtorch/cpu/libtorch-macos-1.4.0.zip)
-and place the libtorch folder under `pytorch/native`
-- Use the following command to build library
+
+- Use the following command to build pytorch JNI library:
+
 ```
-bash build.sh
+./build.sh
 ```
--- pytorch/native/build.sh
+++ pytorch/native/build.sh
@@ -2,8 +2,25 @@
 
 set -e
 
+PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
+if [[ ! -d "libtorch" ]]; then
+  if [[ $PLATFORM == 'linux' ]]; then
+    curl -s https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.4.0%2Bcpu.zip | jar xv
+  elif [[ $PLATFORM == 'darwin' ]]; then
+    curl -s https://download.pytorch.org/libtorch/cpu/libtorch-macos-1.4.0.zip | jar xv
+  else
+    echo "$PLATFORM is not supported."
+    exit 1
+  fi
+fi
+
+pushd .
+
 rm -rf build
 mkdir build && cd build
-javac -cp ../../pytorch-engine/src/main/java/ ../../pytorch-engine/src/main/java/ai/djl/pytorch/jni/PyTorchLibrary.java -h include
+mkdir classes
+javac -sourcepath ../../pytorch-engine/src/main/java/ ../../pytorch-engine/src/main/java/ai/djl/pytorch/jni/PyTorchLibrary.java -h include -d classes
 cmake -DCMAKE_PREFIX_PATH=libtorch ..
 cmake --build . --config Release
+
+popd
