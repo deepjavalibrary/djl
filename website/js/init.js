b9601bd3d146ba05062953e28d96eddd951edf40
Qing <qingla@amazon.com>
2020-05-28 17:43:10 -0700

    add blog section
    Change-Id: Iea78200927ddac6f6e46f5bc2d331ce6fda2ced6

--- website/js/init.js
+++ website/js/init.js
@@ -4,6 +4,7 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.tabs').tabs();
+    $('.collapsible').collapsible();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
