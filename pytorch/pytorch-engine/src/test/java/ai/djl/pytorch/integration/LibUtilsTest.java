91a3547f00480677ecc664cd17b059c30dbdb289
Frank Liu <frankfliu2000@gmail.com>
2020-10-20 20:02:08 -0700

    Workaround #179, allow load shared library from different ClassLoader
    Change-Id: I60ba3469cc841c2bdb2d1696f2b0926c11db1f37

---
+++ pytorch/pytorch-engine/src/test/java/ai/djl/pytorch/integration/LibUtilsTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package ai.djl.pytorch.integration;
+
+import ai.djl.engine.Engine;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LibUtilsTest {
+
+    @BeforeClass
+    public void setup() {
+        System.setProperty(
+                "ai.djl.pytorch.native_helper", "ai.djl.pytorch.integration.LibUtilsTest");
+    }
+
+    @AfterClass
+    public void teardown() {
+        System.setProperty("ai.djl.pytorch.native_helper", "");
+    }
+
+    @Test
+    public void test() {
+        Engine.getInstance();
+    }
+
+    public static void load(String path) {
+        System.load(path); // NOPMD
+    }
+}
