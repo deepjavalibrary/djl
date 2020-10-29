f3700a20654a1fdd33378a9b82433718d25f94fd
Frank Liu <frankfliu2000@gmail.com>
2020-10-29 13:27:38 -0700

    Use MXImperativeInvoke instead of MXImperativeInvokeEx
    Change-Id: I6e6664aa4055086213098b4d0430a013e4289f7a

--- mxnet/mxnet-engine/src/main/java/ai/djl/mxnet/jna/FunctionInfo.java
+++ mxnet/mxnet-engine/src/main/java/ai/djl/mxnet/jna/FunctionInfo.java
@@ -13,7 +13,6 @@
 package ai.djl.mxnet.jna;
 
 import ai.djl.Device;
-import ai.djl.mxnet.engine.MxNDArray;
 import ai.djl.mxnet.engine.MxNDManager;
 import ai.djl.ndarray.NDArray;
 import ai.djl.ndarray.NDManager;
@@ -57,7 +56,8 @@ public class FunctionInfo {
         checkDevices(dest);
         PointerArray srcHandles = JnaUtils.toPointerArray(src);
         PointerByReference destRef = new PointerByReference(JnaUtils.toPointerArray(dest));
-        return JnaUtils.imperativeInvoke(handle, srcHandles, destRef, params).size();
+        JnaUtils.imperativeInvoke(handle, srcHandles, destRef, params);
+        return 0;
     }
 
     /**
@@ -86,18 +86,17 @@ public class FunctionInfo {
      */
     private NDArray[] invoke(MxNDManager manager, PointerArray src, PairList<String, ?> params) {
         PointerByReference destRef = new PointerByReference();
-
-        PairList<Pointer, SparseFormat> pairList =
-                JnaUtils.imperativeInvoke(handle, src, destRef, params);
-        return pairList.stream()
-                .map(
-                        pair -> {
-                            if (pair.getValue() != SparseFormat.DENSE) {
-                                return manager.create(pair.getKey(), pair.getValue());
-                            }
-                            return manager.create(pair.getKey());
-                        })
-                .toArray(MxNDArray[]::new);
+        Pointer[] pointers = JnaUtils.imperativeInvoke(handle, src, destRef, params);
+        NDArray[] ret = new NDArray[pointers.length];
+        for (int i = 0; i < pointers.length; ++i) {
+            SparseFormat format = JnaUtils.getStorageType(pointers[i]);
+            if (format != SparseFormat.DENSE) {
+                ret[i] = manager.create(pointers[i], format);
+            } else {
+                ret[i] = manager.create(pointers[i]);
+            }
+        }
+        return ret;
     }
 
     /**
