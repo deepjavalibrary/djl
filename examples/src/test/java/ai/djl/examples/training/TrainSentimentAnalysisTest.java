af9cf74f6358706263db401c8054414deb3bdd00
Qing Lan <qingla@amazon.com>
2020-10-30 15:15:23 -0700

    apply fix to the sentiment analysis training
    Change-Id: If546762dfa25e51705e9def903bc387e590c93a8

--- examples/src/test/java/ai/djl/examples/training/TrainSentimentAnalysisTest.java
+++ examples/src/test/java/ai/djl/examples/training/TrainSentimentAnalysisTest.java
@@ -10,19 +10,36 @@
  * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
  * and limitations under the License.
  */
-
 package ai.djl.examples.training;
 
+import ai.djl.Device;
+import ai.djl.MalformedModelException;
+import ai.djl.repository.zoo.ModelNotFoundException;
 import ai.djl.training.TrainingResult;
 import ai.djl.translate.TranslateException;
 import java.io.IOException;
 import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 public class TrainSentimentAnalysisTest {
 
     @Test
-    public void testTrainSentimentAnalysis() throws IOException, TranslateException {
+    public void testTrainSentimentAnalysis()
+            throws MalformedModelException, ModelNotFoundException, TranslateException,
+                    IOException {
+        // this is nightly test
+        if (!Boolean.getBoolean("nightly")) {
+            throw new SkipException("Nightly only");
+        }
+        if (Device.getGpuCount() > 0) {
+            String[] args = new String[] {"-e", "1", "-g", "1"};
+            TrainSentimentAnalysis.runExample(args);
+        }
+    }
+
+    @Test
+    public void testTrainSentimentAnalysisSeq2Seq() throws IOException, TranslateException {
         String[] args = new String[] {"-g", "1", "-e", "1", "-m", "2"};
         TrainingResult result = TrainSeq2Seq.runExample(args);
         Assert.assertNotNull(result);
