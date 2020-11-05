b7712718659ee2fb83436b4b0ed818df5f287aae
Frank Liu <frankfliu2000@gmail.com>
2020-11-04 19:43:58 -0800

    Revert "remove MxSparseNDArray"
    This reverts commit 0c4f2de0150f1ce604f2001eb260adb43e1388ec.

--- mxnet/mxnet-engine/src/main/java/ai/djl/mxnet/engine/MxNDArray.java
+++ mxnet/mxnet-engine/src/main/java/ai/djl/mxnet/engine/MxNDArray.java
@@ -271,9 +271,6 @@ public class MxNDArray extends NativeResource implements LazyNDArray {
     /** {@inheritDoc} */
     @Override
     public ByteBuffer toByteBuffer() {
-        if (getSparseFormat() != SparseFormat.DENSE) {
-            throw new IllegalStateException("Please convert sparse NDArray to dense");
-        }
         Shape sh = getShape();
         DataType dType = getDataType();
         long product = sh.size();
@@ -287,9 +284,6 @@ public class MxNDArray extends NativeResource implements LazyNDArray {
     /** {@inheritDoc} */
     @Override
     public void set(Buffer data) {
-        if (getSparseFormat() != SparseFormat.DENSE) {
-            throw new IllegalStateException("Unsupported operation for Sparse");
-        }
         int size = data.remaining();
         // int8, uint8, boolean use ByteBuffer, so need to explicitly input DataType
         DataType inputType = DataType.fromBuffer(data);
