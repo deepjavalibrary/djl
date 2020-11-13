dbae12424b53d808531d61c7839d4930104f0e8f
Frank Liu <frankfliu2000@gmail.com>
2020-11-12 16:59:04 -0800

    Serialize NDArray name
    Change-Id: I52d4866b33fde1b7f09df356790b8b18dec7349e

--- api/src/main/java/ai/djl/ndarray/NDSerializer.java
+++ api/src/main/java/ai/djl/ndarray/NDSerializer.java
@@ -27,7 +27,7 @@ final class NDSerializer {
 
     static final int BUFFER_SIZE = 81920;
     static final String MAGIC_NUMBER = "NDAR";
-    static final int VERSION = 1;
+    static final int VERSION = 2;
 
     private NDSerializer() {}
 
@@ -43,6 +43,13 @@ final class NDSerializer {
             // magic string for version identification
             dos.writeUTF(MAGIC_NUMBER);
             dos.writeInt(VERSION);
+            String name = array.getName();
+            if (name == null) {
+                dos.write(0);
+            } else {
+                dos.write(1);
+                dos.writeUTF(name);
+            }
             dos.writeUTF(array.getSparseFormat().name());
             dos.writeUTF(array.getDataType().name());
 
@@ -90,13 +97,21 @@ final class NDSerializer {
             dis = new DataInputStream(is);
         }
 
+        String name = null;
         // Newer version of NDArray
         if ("NDAR".equals(dis.readUTF())) {
             // NDArray encode version
             int version = dis.readInt();
-            if (version != 1) {
+            if (version < 1 || version > VERSION) {
                 throw new IllegalArgumentException("Unexpected NDArray encode version " + version);
             }
+            if (version > 1) {
+                byte flag = dis.readByte();
+                if (flag == 1) {
+                    name = dis.readUTF();
+                }
+            }
+
             dis.readUTF(); // ignore SparseFormat
         }
         // else ignored as reading SparseFormat for the old version
@@ -123,6 +138,8 @@ final class NDSerializer {
             data.put(buf, 0, length);
             data.rewind();
         }
-        return manager.create(dataType.asDataType(data), shape);
+        NDArray array = manager.create(dataType.asDataType(data), shape);
+        array.setName(name);
+        return array;
     }
 }
