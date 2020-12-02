388d0793248c7ff13ffac9318cc475985d8a8234
Frank Liu <frankfliu2000@gmail.com>
2020-12-02 09:19:38 -0800

    [paddle] change paddle native lib name
    Change-Id: I2735afc16cfa9192e4b42d3cd752ba8bfbfacddc

--- paddlepaddle/paddlepaddle-engine/src/main/java/ai/djl/paddlepaddle/jna/LibUtils.java
+++ paddlepaddle/paddlepaddle-engine/src/main/java/ai/djl/paddlepaddle/jna/LibUtils.java
@@ -47,7 +47,7 @@ public final class LibUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(LibUtils.class);
 
-    private static final String LIB_NAME = "paddle_fluid";
+    private static final String LIB_NAME = "paddle_fluid_c";
     private static final Pattern VERSION_PATTERN =
             Pattern.compile("(\\d+\\.\\d+\\.\\d+)(-SNAPSHOT)?(-\\d+)?");
 
