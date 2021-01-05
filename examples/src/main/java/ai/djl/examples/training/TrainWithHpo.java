84bc03e35ad047ee3c118f285fbbc05e6eb06342
Zach Kimberg <kimbergz@amazon.com>
2021-01-04 16:30:30 -0800

    Create EasyHpo to simplify training with hyperparameters
    Change-Id: If0896dadba41bf84109344fa814c8039c81694dc

--- examples/src/main/java/ai/djl/examples/training/TrainWithHpo.java
+++ examples/src/main/java/ai/djl/examples/training/TrainWithHpo.java
@@ -17,18 +17,14 @@ import ai.djl.Model;
 import ai.djl.basicdataset.Mnist;
 import ai.djl.basicmodelzoo.basic.Mlp;
 import ai.djl.examples.training.util.Arguments;
-import ai.djl.metric.Metrics;
 import ai.djl.ndarray.types.Shape;
 import ai.djl.nn.Block;
 import ai.djl.training.DefaultTrainingConfig;
-import ai.djl.training.EasyTrain;
-import ai.djl.training.Trainer;
 import ai.djl.training.TrainingResult;
-import ai.djl.training.dataset.Dataset;
+import ai.djl.training.dataset.Dataset.Usage;
 import ai.djl.training.dataset.RandomAccessDataset;
 import ai.djl.training.evaluator.Accuracy;
-import ai.djl.training.hyperparameter.optimizer.HpORandom;
-import ai.djl.training.hyperparameter.optimizer.HpOptimizer;
+import ai.djl.training.hyperparameter.EasyHpo;
 import ai.djl.training.hyperparameter.param.HpInt;
 import ai.djl.training.hyperparameter.param.HpSet;
 import ai.djl.training.listener.CheckpointsTrainingListener;
@@ -36,7 +32,6 @@ import ai.djl.training.listener.TrainingListener;
 import ai.djl.training.loss.Loss;
 import ai.djl.training.util.ProgressBar;
 import ai.djl.translate.TranslateException;
-import ai.djl.util.Pair;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -59,38 +54,93 @@ public final class TrainWithHpo {
             return null;
         }
 
-        // get training and validation dataset
-        RandomAccessDataset trainingSet = getDataset(Dataset.Usage.TRAIN, arguments);
-        RandomAccessDataset validateSet = getDataset(Dataset.Usage.TEST, arguments);
+        return new Train(arguments).fit().getValue();
+    }
 
-        HpSet hyperParams =
-                new HpSet(
-                        "hp",
-                        Arrays.asList(
-                                new HpInt("hiddenLayersSize", 10, 100),
-                                new HpInt("hiddenLayersCount", 2, 10)));
-        HpOptimizer hpOptimizer = new HpORandom(hyperParams);
+    static class Train extends EasyHpo {
 
-        final int hyperparameterTests = 50;
+        Arguments arguments;
 
-        for (int i = 0; i < hyperparameterTests; i++) {
-            HpSet hpVals = hpOptimizer.nextConfig();
-            Pair<Model, TrainingResult> trained =
-                    train(arguments, hpVals, trainingSet, validateSet);
-            trained.getKey().close();
-            float loss = trained.getValue().getValidateLoss();
-            hpOptimizer.update(hpVals, loss);
-            logger.info(
-                    "--------- hp test {}/{} - Loss {} - {}", i, hyperparameterTests, loss, hpVals);
+        public Train(Arguments arguments) {
+            this.arguments = arguments;
         }
 
-        HpSet bestHpVals = hpOptimizer.getBest().getKey();
-        Pair<Model, TrainingResult> trained =
-                train(arguments, bestHpVals, trainingSet, validateSet);
-        TrainingResult result = trained.getValue();
-        float loss = result.getValidateLoss();
-        try (Model model = trained.getKey()) {
-            logger.info("--------- FINAL_HP - Loss {} - {}", loss, bestHpVals);
+        @Override
+        protected DefaultTrainingConfig setupTrainingConfig(HpSet hpVals) {
+            String outputDir = arguments.getOutputDir();
+            CheckpointsTrainingListener listener = new CheckpointsTrainingListener(outputDir);
+            listener.setSaveModelCallback(
+                    trainer -> {
+                        TrainingResult result = trainer.getTrainingResult();
+                        Model model = trainer.getModel();
+                        float accuracy = result.getValidateEvaluation("Accuracy");
+                        model.setProperty("Accuracy", String.format("%.5f", accuracy));
+                        model.setProperty("Loss", String.format("%.5f", result.getValidateLoss()));
+                    });
+
+            return new DefaultTrainingConfig(Loss.softmaxCrossEntropyLoss())
+                    .addEvaluator(new Accuracy())
+                    .optDevices(Device.getDevices(arguments.getMaxGpus()))
+                    .addTrainingListeners(TrainingListener.Defaults.logging(outputDir))
+                    .addTrainingListeners(listener);
+        }
+
+        @Override
+        protected HpSet setupHyperParams() {
+            return new HpSet(
+                    "hp",
+                    Arrays.asList(
+                            new HpInt("hiddenLayersSize", 10, 100),
+                            new HpInt("hiddenLayersCount", 2, 10)));
+        }
+
+        @Override
+        protected RandomAccessDataset getDataset(Usage usage) throws IOException {
+            Mnist mnist =
+                    Mnist.builder()
+                            .optUsage(usage)
+                            .setSampling(arguments.getBatchSize(), true)
+                            .optLimit(arguments.getLimit())
+                            .build();
+            mnist.prepare(new ProgressBar());
+            return mnist;
+        }
+
+        @Override
+        protected Model buildModel(HpSet hpVals) {
+            int[] hidden = new int[(Integer) hpVals.getHParam("hiddenLayersCount").random()];
+            Arrays.fill(hidden, (Integer) hpVals.getHParam("hiddenLayersSize").random());
+
+            Block block =
+                    new Mlp(Mnist.IMAGE_HEIGHT * Mnist.IMAGE_WIDTH, Mnist.NUM_CLASSES, hidden);
+            Model model = Model.newInstance("mlp");
+            model.setBlock(block);
+            return model;
+        }
+
+        @Override
+        protected Shape inputShape(HpSet hpVals) {
+            /*
+             * MNIST is 28x28 grayscale image and pre processed into 28 * 28 NDArray.
+             * 1st axis is batch axis, we can use 1 for initialization.
+             */
+            return new Shape(1, Mnist.IMAGE_HEIGHT * Mnist.IMAGE_WIDTH);
+        }
+
+        @Override
+        protected int numEpochs(HpSet hpVals) {
+            return arguments.getEpoch();
+        }
+
+        @Override
+        protected int numHyperParameterTests() {
+            return 50;
+        }
+
+        @Override
+        protected void saveModel(Model model, TrainingResult result) throws IOException {
+            float loss = result.getValidateLoss();
+            logger.info("--------- FINAL_HP - Loss {}", loss);
 
             model.setProperty("Epoch", String.valueOf(result.getEpoch()));
             model.setProperty(
@@ -98,72 +148,5 @@ public final class TrainWithHpo {
             model.setProperty("Loss", String.format("%.5f", loss));
             model.save(Paths.get(arguments.getOutputDir()), "mlp");
         }
-        return result;
-    }
-
-    private static Pair<Model, TrainingResult> train(
-            Arguments arguments,
-            HpSet hpVals,
-            RandomAccessDataset trainingSet,
-            RandomAccessDataset validateSet)
-            throws IOException, TranslateException {
-        // Construct neural network
-        int[] hidden = new int[(Integer) hpVals.getHParam("hiddenLayersCount").random()];
-        Arrays.fill(hidden, (Integer) hpVals.getHParam("hiddenLayersSize").random());
-        Block block = new Mlp(Mnist.IMAGE_HEIGHT * Mnist.IMAGE_WIDTH, Mnist.NUM_CLASSES, hidden);
-        Model model = Model.newInstance("mlp");
-        model.setBlock(block);
-
-        // setup training configuration
-        DefaultTrainingConfig config = setupTrainingConfig(arguments);
-
-        try (Trainer trainer = model.newTrainer(config)) {
-            trainer.setMetrics(new Metrics());
-
-            /*
-             * MNIST is 28x28 grayscale image and pre processed into 28 * 28 NDArray.
-             * 1st axis is batch axis, we can use 1 for initialization.
-             */
-            Shape inputShape = new Shape(1, Mnist.IMAGE_HEIGHT * Mnist.IMAGE_WIDTH);
-
-            // initialize trainer with proper input shape
-            trainer.initialize(inputShape);
-
-            EasyTrain.fit(trainer, arguments.getEpoch(), trainingSet, validateSet);
-
-            TrainingResult result = trainer.getTrainingResult();
-            return new Pair<>(model, result);
-        }
-    }
-
-    private static DefaultTrainingConfig setupTrainingConfig(Arguments arguments) {
-        String outputDir = arguments.getOutputDir();
-        CheckpointsTrainingListener listener = new CheckpointsTrainingListener(outputDir);
-        listener.setSaveModelCallback(
-                trainer -> {
-                    TrainingResult result = trainer.getTrainingResult();
-                    Model model = trainer.getModel();
-                    float accuracy = result.getValidateEvaluation("Accuracy");
-                    model.setProperty("Accuracy", String.format("%.5f", accuracy));
-                    model.setProperty("Loss", String.format("%.5f", result.getValidateLoss()));
-                });
-
-        return new DefaultTrainingConfig(Loss.softmaxCrossEntropyLoss())
-                .addEvaluator(new Accuracy())
-                .optDevices(Device.getDevices(arguments.getMaxGpus()))
-                .addTrainingListeners(TrainingListener.Defaults.logging(outputDir))
-                .addTrainingListeners(listener);
-    }
-
-    private static RandomAccessDataset getDataset(Dataset.Usage usage, Arguments arguments)
-            throws IOException {
-        Mnist mnist =
-                Mnist.builder()
-                        .optUsage(usage)
-                        .setSampling(arguments.getBatchSize(), true)
-                        .optLimit(arguments.getLimit())
-                        .build();
-        mnist.prepare(new ProgressBar());
-        return mnist;
     }
 }
