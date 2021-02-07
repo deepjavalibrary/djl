982cd292aedd56272fb75be69e0cef9cc7897ea3
Tibor Mezei <zemei88@gmail.com>
2021-02-07 15:55:50 -0800

    Various fixes and improvements for MultiThreadedBenchmark (#617)
    * Various fixes and improvements for MultiThreadedBenchmark
    
    * Fix code format and minor logging issue
    
    Change-Id: I476ccfb220f0241ad0647708efd6d42f6316b6f2
    
    Co-authored-by: Frank Liu <frankfliu2000@gmail.com>

--- api/src/main/java/ai/djl/training/listener/MemoryTrainingListener.java
+++ api/src/main/java/ai/djl/training/listener/MemoryTrainingListener.java
@@ -91,12 +91,12 @@ public class MemoryTrainingListener extends TrainingListenerAdapter {
             MemoryUsage heap = memBean.getHeapMemoryUsage();
             MemoryUsage nonHeap = memBean.getNonHeapMemoryUsage();
 
-            long heapCommitted = heap.getCommitted();
-            long nonHeapCommitted = nonHeap.getCommitted();
+            long heapUsed = heap.getUsed();
+            long nonHeapUsed = nonHeap.getUsed();
             getProcessInfo(metrics);
 
-            metrics.addMetric("Heap", heapCommitted, "bytes");
-            metrics.addMetric("NonHeap", nonHeapCommitted, "bytes");
+            metrics.addMetric("Heap", heapUsed, "bytes");
+            metrics.addMetric("NonHeap", nonHeapUsed, "bytes");
             int gpuCount = Device.getGpuCount();
 
             // CudaUtils.getGpuMemory() will allocates memory on GPUs if CUDA runtime is not
