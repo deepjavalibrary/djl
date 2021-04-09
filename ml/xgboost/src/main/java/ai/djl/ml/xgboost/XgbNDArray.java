c7ce8bf885752095b9e8e3da5233bf3cd7e5f16f
Lanking <qingla@amazon.com>
2021-04-09 16:29:17 -0700

    add initial XGBoost changes

---
+++ ml/xgboost/src/main/java/ai/djl/ml/xgboost/XgbNDArray.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package ai.djl.ml.xgboost;
+
+import ai.djl.Device;
+import ai.djl.ndarray.NDArray;
+import ai.djl.ndarray.NDArrayAdapter;
+import ai.djl.ndarray.NDManager;
+import ai.djl.ndarray.types.DataType;
+import ai.djl.ndarray.types.Shape;
+import ai.djl.ndarray.types.SparseFormat;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
+import ml.dmlc.xgboost4j.java.JniUtils;
+
+/** {@code XgbNDArray} is the XGBoost implementation of {@link NDArray}. */
+public class XgbNDArray implements NDArrayAdapter {
+
+    private AtomicLong handle;
+    private String uid;
+    private ByteBuffer data;
+    private XgbNDManager manager;
+    private Shape shape;
+    private boolean isClosed;
+    private SparseFormat format;
+
+    XgbNDArray(XgbNDManager manager, long handle, Shape shape, SparseFormat format) {
+        this.handle = new AtomicLong(handle);
+        this.uid = String.valueOf(handle);
+        this.manager = manager;
+        this.manager.attachInternal(uid, this);
+        this.shape = shape;
+        this.format = format;
+    }
+
+    XgbNDArray(XgbNDManager manager, ByteBuffer data, Shape shape) {
+        this.manager = manager;
+        this.uid = UUID.randomUUID().toString();
+        this.manager.attachInternal(uid, this);
+        this.shape = shape;
+        this.data = data;
+        this.format = SparseFormat.DENSE;
+    }
+
+    /** {@inheritDoc} */
+    public long getHandle() {
+        return handle.get();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public XgbNDManager getManager() {
+        return manager;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getUid() {
+        return uid;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public DataType getDataType() {
+        return DataType.FLOAT32;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Device getDevice() {
+        return Device.cpu();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Shape getShape() {
+        return shape;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public SparseFormat getSparseFormat() {
+        return format;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public ByteBuffer toByteBuffer() {
+        if (data == null) {
+            throw new UnsupportedOperationException("Cannot obtain value from DMatrix");
+        }
+        return data;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void attach(NDManager manager) {
+        detach();
+        this.manager = (XgbNDManager) manager;
+        manager.attachInternal(getUid(), this);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void tempAttach(NDManager manager) {
+        detach();
+        NDManager original = this.manager;
+        this.manager = (XgbNDManager) manager;
+        manager.tempAttachInternal(original, getUid(), this);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void detach() {
+        manager.detachInternal(getUid());
+        manager = XgbNDManager.getSystemManager();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString() {
+        if (isClosed) {
+            return "This array is already closed";
+        }
+        return "ND: "
+                + getShape()
+                + ' '
+                + getDevice()
+                + ' '
+                + getDataType()
+                + '\n'
+                + Arrays.toString(toArray());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void close() {
+        if (handle != null && handle.get() != 0L) {
+            long pointer = handle.getAndSet(0L);
+            JniUtils.deleteDMatrix(pointer);
+        }
+        if (data != null) {
+            data = null;
+        }
+        manager.detachInternal(getUid());
+        isClosed = true;
+    }
+}
---
+++ ml/xgboost/src/main/java/ai/djl/ml/xgboost/XgbSymbolBlock.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package ai.djl.ml.xgboost;
+
+import ai.djl.ndarray.NDList;
+import ai.djl.ndarray.types.Shape;
+import ai.djl.nn.AbstractSymbolBlock;
+import ai.djl.nn.SymbolBlock;
+import ai.djl.training.ParameterStore;
+import ai.djl.util.PairList;
+import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicReference;
+import ml.dmlc.xgboost4j.java.JniUtils;
+
+/** {@code XgbSymbolBlock} is the XGBoost implementation of {@link SymbolBlock}. */
+public class XgbSymbolBlock extends AbstractSymbolBlock implements AutoCloseable {
+
+    private static final byte VERSION = 1;
+    private AtomicReference<Long> handle;
+    private String uid;
+    private XgbNDManager manager;
+
+    /**
+     * Constructs a {@code XgbSymbolBlock}.
+     *
+     * <p>You can create a {@code XgbSymbolBlock} using {@link ai.djl.Model#load(java.nio.file.Path,
+     * String)}.
+     *
+     * @param manager the manager to use for the block
+     * @param handle the Booster handle
+     */
+    public XgbSymbolBlock(XgbNDManager manager, long handle) {
+        super(VERSION);
+        this.handle = new AtomicReference<>(handle);
+        this.manager = manager;
+        uid = String.valueOf(handle);
+        manager.attachInternal(uid, this);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected NDList forwardInternal(
+            ParameterStore parameterStore,
+            NDList inputs,
+            boolean training,
+            PairList<String, Object> params) {
+        Mode mode = Mode.DEFAULT;
+        int treeLimit = 0;
+        if (params != null) {
+            if (params.contains("Mode")) {
+                mode = (Mode) params.get("Mode");
+            }
+            if (params.contains("TreeLimit")) {
+                treeLimit = (int) params.get("TreeLimit");
+            }
+        }
+        XgbNDArray array = (XgbNDArray) inputs.singletonOrThrow();
+        float[] result = JniUtils.inference(this, array, treeLimit, mode);
+        ByteBuffer buf = XgbNDManager.getSystemManager().allocateDirect(result.length * 4);
+        buf.asFloatBuffer().put(result);
+        buf.rewind();
+        return new NDList(new XgbNDArray(array.getManager(), buf, new Shape(result.length)));
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void close() {
+        Long pointer = handle.getAndSet(null);
+        if (pointer != null) {
+            JniUtils.deleteModel(pointer);
+            manager.detachInternal(uid);
+            manager = null;
+        }
+    }
+
+    /**
+     * Gets the native XGBoost Booster pointer.
+     *
+     * @return the pointer
+     */
+    public Long getHandle() {
+        Long reference = handle.get();
+        if (reference == null) {
+            throw new IllegalStateException("XGBoost model handle has been released!");
+        }
+        return reference;
+    }
+
+    /** The mode of inference for OptionMask. */
+    public enum Mode {
+        DEFAULT(0),
+        OUTPUT_MARGIN(1),
+        LEAF(2),
+        CONTRIB(4);
+
+        private int value;
+
+        Mode(int value) {
+            this.value = value;
+        }
+
+        /**
+         * Gets the value of the mode.
+         *
+         * @return the value in number
+         */
+        public int getValue() {
+            return value;
+        }
+    }
+}
