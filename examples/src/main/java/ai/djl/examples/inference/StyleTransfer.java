4ea4d9d6504777f96d392530f6184edaf6a7db9f
Aziz Zayed <azayed01@gmail.com>
2021-08-21 21:34:08 -0700

    Change directory for StyleTranfer example

--- examples/src/main/java/ai/djl/examples/inference/StyleTransfer.java
+++ examples/src/main/java/ai/djl/examples/inference/StyleTransfer.java
@@ -10,7 +10,7 @@
  * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
  * and limitations under the License.
  */
-package ai.djl.examples.inference.cyclegan;
+package ai.djl.examples.inference;
 
 import ai.djl.Application;
 import ai.djl.MalformedModelException;
