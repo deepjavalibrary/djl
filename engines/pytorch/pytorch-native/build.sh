0753fb6256bc5765adc59f5e7163b77453a0fe27
Frank Liu <frankfliu2000@gmail.com>
2021-10-24 11:57:08 -0700

    [ci] build pytorch jni for different version
    Change-Id: I6d8d82e625f09bb8f80b32e2e4a07d1f875ca500

--- engines/pytorch/pytorch-native/build.sh
+++ engines/pytorch/pytorch-native/build.sh
@@ -5,22 +5,23 @@ WORK_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export WORK_DIR
 NUM_PROC=1
 if [[ -n $(command -v nproc) ]]; then
-    NUM_PROC=$(nproc)
+  NUM_PROC=$(nproc)
 elif [[ -n $(command -v sysctl) ]]; then
-    NUM_PROC=$(sysctl -n hw.ncpu)
+  NUM_PROC=$(sysctl -n hw.ncpu)
 fi
 
 PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
-VERSION=1.9.1
+VERSION=$1
+FLAVOR=$2
 CXX11ABI="-cxx11-abi"
-if [[ $2 == "precxx11" ]]; then
+if [[ $3 == "precxx11" ]]; then
   CXX11ABI=""
 fi
 
 if [[ ! -d "libtorch" ]]; then
   if [[ $PLATFORM == 'linux' ]]; then
-    if [[ $1 == "cpu" ]] ||  [[ $1 == "cu111" ]] || [[ $1 == "cu102" ]]; then
-      curl -s https://download.pytorch.org/libtorch/${1}/libtorch${CXX11ABI}-shared-with-deps-${VERSION}%2B${1}.zip | jar xv
+    if [[ $FLAVOR == "cpu" ]] || [[ $FLAVOR == "cu111" ]] || [[ $FLAVOR == "cu102" ]]; then
+      curl -s https://download.pytorch.org/libtorch/${FLAVOR}/libtorch${CXX11ABI}-shared-with-deps-${VERSION}%2B${FLAVOR}.zip | jar xv
     else
       echo "$1 is not supported."
       exit 1
@@ -43,7 +44,7 @@ cmake -DCMAKE_PREFIX_PATH=libtorch ..
 cmake --build . --config Release -- -j "${NUM_PROC}"
 
 if [[ $PLATFORM == 'darwin' ]]; then
-  install_name_tool -add_rpath  @loader_path libdjl_torch.dylib
+  install_name_tool -add_rpath @loader_path libdjl_torch.dylib
 fi
 
 popd
