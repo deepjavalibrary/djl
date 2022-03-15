8b18f008659bcbab180add4c7c1af2a8cd8edef4
Zach Kimberg <kimbergz@amazon.com>
2022-03-15 09:22:12 -0700

    Parse default device with Device.fromName
    This makes the names "" and null return the default device (with potentially
    specified engine) instead of CPU.

--- api/src/test/java/ai/djl/DeviceTest.java
+++ api/src/test/java/ai/djl/DeviceTest.java
@@ -40,9 +40,7 @@ public class DeviceTest {
     @Test
     public void testDeviceName() {
         Assert.assertEquals(Device.fromName("cpu"), Device.cpu());
-        Assert.assertEquals(Device.fromName(""), Device.cpu());
         Assert.assertEquals(Device.fromName("-1"), Device.cpu());
-        Assert.assertEquals(Device.fromName(null), Device.cpu());
 
         Assert.assertEquals(Device.fromName("gpu0"), Device.gpu());
         Assert.assertEquals(Device.fromName("0"), Device.gpu());
@@ -50,5 +48,9 @@ public class DeviceTest {
 
         Assert.assertEquals(Device.fromName("nc1"), Device.of("nc", 1));
         Assert.assertEquals(Device.fromName("a999"), Device.of("a", 999));
+
+        Device defaultDevice = Engine.getInstance().defaultDevice();
+        Assert.assertEquals(Device.fromName(""), defaultDevice);
+        Assert.assertEquals(Device.fromName(null), defaultDevice);
     }
 }
