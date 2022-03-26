8cf238f38a6098aaee6438afeaa35865870e5f80
WHALEEYE <1094349973@qq.com>
2022-03-26 17:25:28 +0800

    [basicdataset] Add WikiText-2 dataset

---
+++ basicdataset/src/main/java/ai/djl/basicdataset/nlp/WikiText2.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package ai.djl.basicdataset.nlp;
+
+import ai.djl.Application;
+import ai.djl.basicdataset.BasicDatasets;
+import ai.djl.basicdataset.RawDataset;
+import ai.djl.ndarray.NDManager;
+import ai.djl.repository.Artifact;
+import ai.djl.repository.MRL;
+import ai.djl.repository.Repository;
+import ai.djl.training.dataset.Batch;
+import ai.djl.training.dataset.Dataset;
+import ai.djl.translate.TranslateException;
+import ai.djl.util.Progress;
+import java.io.IOException;
+import java.nio.file.Path;
+
+/**
+ * The WikiText language modeling dataset is a collection of over 100 million tokens extracted from
+ * the set of verified Good and Featured articles on Wikipedia.
+ */
+public class WikiText2 implements RawDataset<Path> {
+
+    private static final String VERSION = "1.0";
+    private static final String ARTIFACT_ID = "WikiText-2";
+
+    private Dataset.Usage usage;
+    private Path root;
+
+    private MRL mrl;
+    private boolean prepared;
+
+    WikiText2(Builder builder) {
+        this.usage = builder.usage;
+        mrl = builder.getMrl();
+    }
+
+    /**
+     * Creates a builder to build a {@link WikiText2}.
+     *
+     * @return a new {@link WikiText2.Builder} object
+     */
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    /**
+     * Prepares the dataset for use with tracked progress.
+     *
+     * @param progress the progress tracker
+     * @throws IOException for various exceptions depending on the dataset
+     */
+    @Override
+    public void prepare(Progress progress) throws IOException {
+        if (prepared) {
+            return;
+        }
+
+        Artifact artifact = mrl.getDefaultArtifact();
+        mrl.prepare(artifact, progress);
+        Artifact.Item item;
+        item = artifact.getFiles().get("WikiText-2");
+        String path;
+        switch (usage) {
+            case TRAIN:
+                path = "wikitext-2/wiki.train.tokens";
+                break;
+            case TEST:
+                path = "wikitext-2/wiki.test.tokens";
+                break;
+            case VALIDATION:
+                path = "wikitext-2/wiki.valid.tokens";
+                break;
+            default:
+                throw new UnsupportedOperationException("Unsupported usage type.");
+        }
+
+        root = mrl.getRepository().getFile(item, path).toAbsolutePath();
+        prepared = true;
+    }
+
+    /**
+     * Fetches an iterator that can iterate through the {@link Dataset}. This method is not
+     * implemented for the WikiText2 dataset because the WikiText2 dataset is not suitable for
+     * iteration. If the method is called, it will directly return {@code null}.
+     *
+     * @param manager the dataset to iterate through
+     * @return an {@link Iterable} of {@link Batch} that contains batches of data from the dataset
+     */
+    @Override
+    public Iterable<Batch> getData(NDManager manager) throws IOException, TranslateException {
+        return null;
+    }
+
+    /**
+     * Get data from the WikiText2 dataset. This method will directly return the whole dataset.
+     *
+     * @return a {@link Path} object locating the WikiText2 dataset file
+     */
+    @Override
+    public Path getData() throws IOException {
+        prepare(null);
+        return root;
+    }
+
+    /** A builder to construct a {@link WikiText2} . */
+    public static final class Builder {
+
+        Repository repository;
+        String groupId;
+        String artifactId;
+        Dataset.Usage usage;
+
+        /** Constructs a new builder. */
+        Builder() {
+            repository = BasicDatasets.REPOSITORY;
+            groupId = BasicDatasets.GROUP_ID;
+            artifactId = ARTIFACT_ID;
+            usage = Dataset.Usage.TRAIN;
+        }
+
+        /**
+         * Sets the optional repository for the dataset.
+         *
+         * @param repository the new repository
+         * @return this builder
+         */
+        public Builder optRepository(Repository repository) {
+            this.repository = repository;
+            return this;
+        }
+
+        /**
+         * Sets optional groupId.
+         *
+         * @param groupId the groupId
+         * @return this builder
+         */
+        public Builder optGroupId(String groupId) {
+            this.groupId = groupId;
+            return this;
+        }
+
+        /**
+         * Sets the optional artifactId.
+         *
+         * @param artifactId the artifactId
+         * @return this builder
+         */
+        public Builder optArtifactId(String artifactId) {
+            if (artifactId.contains(":")) {
+                String[] tokens = artifactId.split(":");
+                groupId = tokens[0];
+                this.artifactId = tokens[1];
+            } else {
+                this.artifactId = artifactId;
+            }
+            return this;
+        }
+
+        /**
+         * Sets the optional usage for the dataset.
+         *
+         * @param usage the usage
+         * @return this builder
+         */
+        public Builder optUsage(Dataset.Usage usage) {
+            this.usage = usage;
+            return this;
+        }
+
+        /**
+         * Builds a new {@link WikiText2} object.
+         *
+         * @return the new {@link WikiText2} object
+         */
+        public WikiText2 build() {
+            return new WikiText2(this);
+        }
+
+        MRL getMrl() {
+            return repository.dataset(Application.NLP.ANY, groupId, artifactId, VERSION);
+        }
+    }
+}
---
+++ basicdataset/src/test/java/ai/djl/basicdataset/WikiText2Test.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package ai.djl.basicdataset;
+
+import ai.djl.basicdataset.nlp.WikiText2;
+import ai.djl.repository.Repository;
+import ai.djl.training.dataset.Dataset;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class WikiText2Test {
+    @Test
+    public void testWikiTest2TrainLocal() throws IOException {
+        Repository repository = Repository.newInstance("test", "src/test/resources/mlrepo");
+
+        WikiText2 trainingSet =
+                WikiText2.builder().optRepository(repository).optUsage(Dataset.Usage.TRAIN).build();
+        Path path = trainingSet.getData();
+        Assert.assertTrue(Files.isRegularFile(path));
+    }
+
+    @Test
+    public void testWikiTest2TestLocal() throws IOException {
+        Repository repository = Repository.newInstance("test", "src/test/resources/mlrepo");
+
+        WikiText2 trainingSet =
+                WikiText2.builder().optRepository(repository).optUsage(Dataset.Usage.TEST).build();
+        Path path = trainingSet.getData();
+        Assert.assertTrue(Files.isRegularFile(path));
+    }
+
+    @Test
+    public void testWikiTest2ValidationLocal() throws IOException {
+        Repository repository = Repository.newInstance("test", "src/test/resources/mlrepo");
+
+        WikiText2 trainingSet =
+                WikiText2.builder()
+                        .optRepository(repository)
+                        .optUsage(Dataset.Usage.VALIDATION)
+                        .build();
+        Path path = trainingSet.getData();
+        Assert.assertTrue(Files.isRegularFile(path));
+    }
+}
