c18e3cceed6a05e10ea3ee1d6e88351d0cb04d42
konata <2248548722@qq.com>
2022-04-20 22:41:02 +0800

    [basicdataset] Add GoEmotions dataset

---
+++ basicdataset/src/test/java/ai/djl/basicdataset/GoEmotionsTest.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package ai.djl.basicdataset;
+
+import ai.djl.basicdataset.tabular.GoEmotions;
+import ai.djl.ndarray.NDManager;
+import ai.djl.repository.Repository;
+import ai.djl.training.dataset.Dataset;
+import ai.djl.training.dataset.Record;
+import ai.djl.translate.TranslateException;
+import java.io.IOException;
+import java.util.List;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GoEmotionsTest {
+    @Test
+    public void testGoEmotionTrainRemote() throws IOException, TranslateException {
+        GoEmotions trainingSet =
+                GoEmotions.builder().optUsage(Dataset.Usage.TRAIN).setSampling(32, true).build();
+        trainingSet.prepare();
+
+        long size = trainingSet.size();
+        Assert.assertEquals(size, 43410);
+
+        NDManager manager = NDManager.newBaseManager();
+        Record record = trainingSet.get(manager, 0);
+        Assert.assertEquals(record.getData().get(0).getFloat(), 0);
+        Assert.assertEquals(record.getLabels().get(0).getFloat(), 27.0);
+    }
+
+    @Test
+    public void testGoEmotionTrainLocal() throws IOException, TranslateException {
+        Repository repository = Repository.newInstance("test", "src/test/resources/mlrepo");
+
+        GoEmotions trainingSet =
+                GoEmotions.builder()
+                        .optRepository(repository)
+                        .optUsage(Dataset.Usage.TRAIN)
+                        .setSampling(32, true)
+                        .optArtifactId("goemotions")
+                        .build();
+        List<String> l = GoEmotions.builder().getAvailableFeatures();
+        trainingSet.prepare();
+
+        long size = trainingSet.size();
+        Assert.assertEquals(size, 43410);
+
+        NDManager manager = NDManager.newBaseManager();
+        Record record = trainingSet.get(manager, 0);
+        Assert.assertEquals(record.getData().get(0).getFloat(), 0);
+        Assert.assertEquals(record.getData().get(0).getShape().get(0), 1);
+        Assert.assertEquals(record.getLabels().get(0).getFloat(), 27.0);
+    }
+
+    @Test
+    public void testGoEmotionTestLocal() throws IOException, TranslateException {
+        Repository repository = Repository.newInstance("test", "src/test/resources/mlrepo");
+
+        GoEmotions trainingSet =
+                GoEmotions.builder()
+                        .optRepository(repository)
+                        .optUsage(Dataset.Usage.TEST)
+                        .setSampling(32, true)
+                        .build();
+        trainingSet.prepare();
+
+        long size = trainingSet.size();
+        Assert.assertEquals(size, 5427);
+
+        NDManager manager = NDManager.newBaseManager();
+        Record record = trainingSet.get(manager, 0);
+        Assert.assertEquals(record.getData().get(0).getFloat(), 0);
+        Assert.assertEquals(record.getData().get(0).getShape().get(0), 1);
+        Assert.assertEquals(record.getLabels().get(0).getFloat(), 25.0);
+    }
+
+    @Test
+    public void testGoEmotionValidationLocal() throws IOException, TranslateException {
+        Repository repository = Repository.newInstance("test", "src/test/resources/mlrepo");
+
+        GoEmotions trainingSet =
+                GoEmotions.builder()
+                        .optRepository(repository)
+                        .optUsage(Dataset.Usage.VALIDATION)
+                        .setSampling(32, true)
+                        .build();
+        trainingSet.prepare();
+
+        long size = trainingSet.size();
+        Assert.assertEquals(size, 5426);
+
+        NDManager manager = NDManager.newBaseManager();
+        Record record = trainingSet.get(manager, 0);
+        Assert.assertEquals(record.getData().get(0).getFloat(), 0);
+        Assert.assertEquals(record.getData().get(0).getShape().get(0), 1);
+        Assert.assertEquals(record.getLabels().get(0).getFloat(), 27.0);
+    }
+}
