df7b963dcbdba77b0b456572574f5f88e0d7bea5
gforman44 <gforman44@users.noreply.github.com>
2022-05-14 00:12:58 -0700

    Create Coverage.java
    For regression problems, you'd like to know how often you're over-estimating the target.   Sometimes you want to overestimate, say, 75% of the time, e.g. to make sure you have enough milk in stock to satisfy your customers.

---
+++ api/src/main/java/ai/djl/training/evaluator/Coverage.java
@@ -0,0 +1,34 @@
+package ai.djl.training.evaluator;
+
+import ai.djl.ndarray.NDArray;
+import ai.djl.ndarray.NDList;
+import ai.djl.util.Pair;
+
+/** Coverage for a Regression problem:
+ * it measures the percent of predictions greater than the actual target,
+ * to determine whether the predictor is over-forecasting or under-forecasting.
+ * e.g. 0.50 if we predict near the median of the distribution.
+ * <pre>
+ *  def coverage(target, forecast):
+ *     return (np.mean((target < forecast)))
+ * </pre>
+ * https://bibinmjose.github.io/2021/03/08/errorblog.html
+ */
+public class Coverage extends AbstractAccuracy {
+
+    public Coverage() {
+        this("Coverage", 1);
+    }
+
+    public Coverage(String name, int axis) {
+        super(name, axis);
+    }
+
+    @Override
+    protected Pair<Long, NDArray> accuracyHelper(NDList labels, NDList predictions) {
+        NDArray labl = labels.head();
+        NDArray pred = predictions.head();
+        return new Pair<Long,NDArray>((Long) labl.size(), labl.lt(pred));
+    }
+
+}
