6010b2d7631a8c2752ec6f6bfb1dfd35d49ff53c
Daniel Xu <11912921@mail.sustech.edu.cn>
2022-05-28 21:55:39 +0800

    Add copyright info and package-info

--- extensions/audio/src/main/java/ai/djl/audio/dataset/package-info.java
+++ extensions/audio/src/main/java/ai/djl/audio/dataset/package-info.java
@@ -11,11 +11,5 @@
  * and limitations under the License.
  */
 
-package ai.djl.audio.featurizer;
-
-import ai.djl.ndarray.NDArray;
-import ai.djl.ndarray.NDManager;
-
-public interface AudioProcessor {
-    NDArray extractFeatures(NDManager manager, NDArray samples);
-}
+/** Contains useful audio datasets */
+package ai.djl.audio.dataset;
