c6d2efe9c9baeeab20700ce1dd972662c9e4c564
Frank Liu <frankfliu2000@gmail.com>
2022-05-30 08:56:09 -0700

    minor license header fix
    Change-Id: I91586c6ac93909931da57946e3fb0510661fa4c6

--- examples/src/test/java/ai/djl/examples/training/TrainBertOnGoemotionsTest.java
+++ examples/src/test/java/ai/djl/examples/training/TrainBertOnGoemotionsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
  * with the License. A copy of the License is located at
@@ -18,6 +18,7 @@ import java.io.IOException;
 import org.testng.annotations.Test;
 
 public class TrainBertOnGoemotionsTest {
+
     @Test
     public void testTrainBert() throws IOException, TranslateException {
         TestRequirements.engine("MXNet", "PyTorch");
