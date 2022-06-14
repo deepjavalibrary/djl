87e1e87ee73b93a6f3b02a2779e3bd38230183f0
Frank Liu <frankfliu2000@gmail.com>
2022-06-14 09:09:40 -0700

    format the code
    Change-Id: I436c75a06511e197f6abf923228f53af06d6bbd6

--- android/core/src/androidTest/java/ai/djl/android/core/ModelLoadingTest.java
+++ android/core/src/androidTest/java/ai/djl/android/core/ModelLoadingTest.java
@@ -12,11 +12,12 @@
  */
 package ai.djl.android.core;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
 
-import ai.djl.MalformedModelException;
+import ai.djl.ModelException;
 import ai.djl.inference.Predictor;
 import ai.djl.modality.Classifications;
 import ai.djl.modality.cv.Image;
@@ -24,30 +25,31 @@ import ai.djl.modality.cv.ImageFactory;
 import ai.djl.modality.cv.transform.ToTensor;
 import ai.djl.modality.cv.translator.ImageClassificationTranslator;
 import ai.djl.repository.zoo.Criteria;
-import ai.djl.repository.zoo.ModelNotFoundException;
 import ai.djl.repository.zoo.ModelZoo;
 import ai.djl.repository.zoo.ZooModel;
 import ai.djl.translate.TranslateException;
 
 /* Integration test to check if model and inference runs fine */
 public class ModelLoadingTest {
-        @Test
-        public void testModelLoading() throws IOException, ModelNotFoundException, MalformedModelException, TranslateException {
-            ImageClassificationTranslator translator = ImageClassificationTranslator.builder()
-                    .addTransform(new ToTensor())
-                    .optFlag(Image.Flag.GRAYSCALE)
-                    .optApplySoftmax(true)
-                    .build();
-            Criteria<Image, Classifications> criteria =
-                    Criteria.builder()
-                            .setTypes(Image.class, Classifications.class)
-                            .optModelUrls("https://djl-ai.s3.amazonaws.com/resources/demo/pytorch/doodle_mobilenet.zip")
-                            .optTranslator(translator)
-                            .build();
-            Image image = ImageFactory.getInstance().fromUrl("https://github.com/deepjavalibrary/djl/raw/master/examples/src/test/resources/dog_bike_car.jpg");
-            ZooModel<Image, Classifications> model =  ModelZoo.loadModel(criteria);
-            Predictor<Image, Classifications> predictor = model.newPredictor();
+
+    @Test
+    public void testModelLoading() throws IOException, ModelException, TranslateException {
+        ImageClassificationTranslator translator = ImageClassificationTranslator.builder()
+                .addTransform(new ToTensor())
+                .optFlag(Image.Flag.GRAYSCALE)
+                .optApplySoftmax(true)
+                .build();
+        Criteria<Image, Classifications> criteria =
+                Criteria.builder()
+                        .setTypes(Image.class, Classifications.class)
+                        .optModelUrls("https://djl-ai.s3.amazonaws.com/resources/demo/pytorch/doodle_mobilenet.zip")
+                        .optTranslator(translator)
+                        .build();
+        Image image = ImageFactory.getInstance().fromUrl("https://resources.djl.ai/images/0.png");
+        try (ZooModel<Image, Classifications> model = ModelZoo.loadModel(criteria);
+             Predictor<Image, Classifications> predictor = model.newPredictor()) {
             Classifications result = predictor.predict(image);
-            System.out.println(result);
+            Assert.assertEquals("dog", result.best().getClassName());
         }
+    }
 }
