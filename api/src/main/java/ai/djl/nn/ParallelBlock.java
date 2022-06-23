4b16e8c53c925d2ba3ef0c95aa021b749839138e
Joerg Kiegeland <joerg@kiegeland.com>
2022-06-23 20:45:23 +1000

    use same add methods in ParallelBock like in SequentialBlock

--- api/src/main/java/ai/djl/nn/ParallelBlock.java
+++ api/src/main/java/ai/djl/nn/ParallelBlock.java
@@ -13,6 +13,7 @@
 package ai.djl.nn;
 
 import ai.djl.MalformedModelException;
+import ai.djl.ndarray.NDArray;
 import ai.djl.ndarray.NDList;
 import ai.djl.ndarray.NDManager;
 import ai.djl.ndarray.types.DataType;
@@ -110,6 +111,43 @@ public class ParallelBlock extends AbstractBlock {
      */
     public final ParallelBlock add(Function<NDList, NDList> f) {
         return add(new LambdaBlock(f));
+    }
+
+    /**
+     * Adds a {@link LambdaBlock}, that applies the given function, to the list of parallel
+     * branches.
+     *
+     * @param f the function forms the {@link LambdaBlock}
+     * @param name the function name
+     * @return this block
+     */
+    public ParallelBlock add(Function<NDList, NDList> f, String name) {
+        return add(new LambdaBlock(f, name));
+    }
+
+    /**
+     * Adds a {@link LambdaBlock#singleton(Function)}, that applies the given function, to the list
+     * of parallel branches.
+     *
+     * @param f the function forms the {@link LambdaBlock}
+     * @return this block
+     * @see LambdaBlock#singleton(Function)
+     */
+    public ParallelBlock addSingleton(Function<NDArray, NDArray> f) {
+        return add(LambdaBlock.singleton(f));
+    }
+
+    /**
+     * Adds a {@link LambdaBlock#singleton(Function)}, that applies the given function, to the list
+     * of parallel branches.
+     *
+     * @param f the function forms the {@link LambdaBlock}
+     * @param name the function name
+     * @return this block
+     * @see LambdaBlock#singleton(Function)
+     */
+    public ParallelBlock addSingleton(Function<NDArray, NDArray> f, String name) {
+        return add(LambdaBlock.singleton(f, name));
     }
 
     /** {@inheritDoc} */
