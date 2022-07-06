5923d95f668281f941211da085eb796a92aaa5a0
John Doll <jdollmu@gmail.com>
2022-07-06 13:35:55 -0700

    Adding semantic segmentation example (#1764)
    * Adding semantic segmentation example
    
    * Fixed variables to match styling
    
    * Changing variables to static per convention
    
    * Updating variables to match static naming convention
    
    * Updated copyright dates
    
    Co-authored-by: Frank Liu <frankfliu2000@gmail.com>
    
    * Removed "final" keyword for readability
    
    * Adding line break to pass style checks
    
    Ran ./gradlew fJ and this was the fix
    
    * Use image from S3 bucket
    
    Change-Id: Iec3943455cb58dc0d8644841446cb3d91242bb20
    
    * adding resize transform
    
    Co-authored-by: Frank Liu <frankfliu2000@gmail.com>

---
+++ examples/src/main/java/ai/djl/examples/inference/SemanticSegmentation.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package ai.djl.examples.inference;
+
+import ai.djl.ModelException;
+import ai.djl.inference.Predictor;
+import ai.djl.modality.cv.Image;
+import ai.djl.modality.cv.ImageFactory;
+import ai.djl.modality.cv.translator.SemanticSegmentationTranslator;
+import ai.djl.repository.zoo.Criteria;
+import ai.djl.repository.zoo.ZooModel;
+import ai.djl.training.util.ProgressBar;
+import ai.djl.translate.TranslateException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * An example of inference using a semantic segmentation model.
+ *
+ * <p>See this <a
+ * href="https://github.com/deepjavalibrary/djl/blob/master/examples/docs/semantic_segmentation.md">doc</a>
+ * for information about this example.
+ */
+public final class SemanticSegmentation {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(SemanticSegmentationTranslator.class);
+
+    private SemanticSegmentation() {}
+
+    public static void main(String[] args) throws IOException, ModelException, TranslateException {
+        SemanticSegmentation.predict();
+    }
+
+    public static void predict() throws IOException, ModelException, TranslateException {
+        Path imageFile = Paths.get("src/test/resources/dog_bike_car.jpg");
+        Image img = ImageFactory.getInstance().fromFile(imageFile);
+
+        int height = img.getHeight();
+        int width = img.getWidth();
+
+        String url =
+                "https://mlrepo.djl.ai/model/cv/semantic_segmentation/ai/djl/pytorch/deeplabv3/0.0.1/deeplabv3.zip";
+        Map<String, String> arguments = new ConcurrentHashMap<>();
+        arguments.put("toTensor", "true");
+        arguments.put("normalize", "true");
+        arguments.put("resize", "true");
+        arguments.put("width", String.valueOf(width));
+        arguments.put("height", String.valueOf(height));
+        SemanticSegmentationTranslator translator =
+                SemanticSegmentationTranslator.builder(arguments).build();
+
+        Criteria<Image, Image> criteria =
+                Criteria.builder()
+                        .setTypes(Image.class, Image.class)
+                        .optModelUrls(url)
+                        .optTranslator(translator)
+                        .optEngine("PyTorch")
+                        .optProgress(new ProgressBar())
+                        .build();
+        try (ZooModel<Image, Image> model = criteria.loadModel()) {
+            try (Predictor<Image, Image> predictor = model.newPredictor()) {
+                Image semanticImage = predictor.predict(img);
+                saveSemanticImage(semanticImage);
+            }
+        }
+    }
+
+    private static void saveSemanticImage(Image img) throws IOException {
+        Path outputDir = Paths.get("build/output");
+        Files.createDirectories(outputDir);
+
+        Path imagePath = outputDir.resolve("semantic_instances.png");
+        img.save(Files.newOutputStream(imagePath), "png");
+        logger.info("Segmentation result image has been saved in: {}", imagePath);
+    }
+}
