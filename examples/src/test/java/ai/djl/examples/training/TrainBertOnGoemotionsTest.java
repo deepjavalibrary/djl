f5b990b3b84d3da470a9bf5b6e181af9ef0a0ea7
Frank Liu <frankfliu2000@gmail.com>
2022-08-14 11:56:03 -0700

    [log4j] Upgrade log4j to 2.18.0 (#1904)
    Also fixed existing java format issues
    
    Change-Id: I3c8758bf2925035f57bc63e91ca9bc452a45b092

--- examples/src/test/java/ai/djl/examples/training/TrainBertOnGoemotionsTest.java
+++ examples/src/test/java/ai/djl/examples/training/TrainBertOnGoemotionsTest.java
@@ -14,8 +14,10 @@ package ai.djl.examples.training;
 
 import ai.djl.testing.TestRequirements;
 import ai.djl.translate.TranslateException;
-import java.io.IOException;
+
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class TrainBertOnGoemotionsTest {
 
