d6a2b0606ac1cb433479aaaed7f76914821616dd
KexinFeng <fengx463@umn.edu>
2022-08-25 00:53:31 -0700

    Add test

--- api/src/main/java/ai/djl/training/evaluator/Coverage.java
+++ api/src/main/java/ai/djl/training/evaluator/Coverage.java
@@ -4,15 +4,17 @@ import ai.djl.ndarray.NDArray;
 import ai.djl.ndarray.NDList;
 import ai.djl.util.Pair;
 
-/** Coverage for a Regression problem:
- * it measures the percent of predictions greater than the actual target,
- * to determine whether the predictor is over-forecasting or under-forecasting.
- * e.g. 0.50 if we predict near the median of the distribution.
+/**
+ * Coverage for a Regression problem: it measures the percent of predictions greater than the actual
+ * target, to determine whether the predictor is over-forecasting or under-forecasting. e.g. 0.50 if
+ * we predict near the median of the distribution.
+ *
  * <pre>
  *  def coverage(target, forecast):
- *     return (np.mean((target < forecast)))
+ *     return (np.mean((target &lt; forecast)))
  * </pre>
- * https://bibinmjose.github.io/2021/03/08/errorblog.html
+ *
+ * <a href="https://bibinmjose.github.io/2021/03/08/errorblog.html">...</a>
  */
 public class Coverage extends AbstractAccuracy {
 
@@ -28,7 +30,6 @@ public class Coverage extends AbstractAccuracy {
     protected Pair<Long, NDArray> accuracyHelper(NDList labels, NDList predictions) {
         NDArray labl = labels.head();
         NDArray pred = predictions.head();
-        return new Pair<Long,NDArray>((Long) labl.size(), labl.lt(pred));
+        return new Pair<>(labl.size(), labl.lt(pred));
     }
-
 }
