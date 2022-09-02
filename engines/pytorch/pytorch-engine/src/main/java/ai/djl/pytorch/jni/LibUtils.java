f8ff0f21df0bdf5f2fd4d79de295fd14b3155991
demq <hmatevosyan@yahoo.com>
2022-09-02 16:49:32 +0930

    Update engines/pytorch/pytorch-engine/src/main/java/ai/djl/pytorch/jni/LibUtils.java
    Co-authored-by: Frank Liu <frankfliu2000@gmail.com>

--- engines/pytorch/pytorch-engine/src/main/java/ai/djl/pytorch/jni/LibUtils.java
+++ engines/pytorch/pytorch-engine/src/main/java/ai/djl/pytorch/jni/LibUtils.java
@@ -23,7 +23,6 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.lang.reflect.Method;
 import java.net.URL;
