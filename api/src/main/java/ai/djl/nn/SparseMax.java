dbb81ec1a233eee576405ab1fe7d33bd251b7a9d
warthecatalyst <18317011442@163.com>
2022-09-19 17:41:37 +0800

    finish SparseMax

--- api/src/main/java/ai/djl/nn/SparseMax.java
+++ api/src/main/java/ai/djl/nn/SparseMax.java
@@ -4,32 +4,39 @@ import ai.djl.ndarray.NDArray;
 import ai.djl.ndarray.NDArrays;
 import ai.djl.ndarray.NDList;
 import ai.djl.ndarray.NDManager;
+import ai.djl.ndarray.types.DataType;
 import ai.djl.ndarray.types.Shape;
 import ai.djl.training.ParameterStore;
 import ai.djl.util.PairList;
 
+import java.util.stream.IntStream;
+
 /**
  * {@code SparseMax} contains a generic implementation of sparsemax function
- * the definition of SparseMax can be referred to https://arxiv.org/pdf/1602.02068.pdf
- * this implementation is a simpler implementation of sparseMax function,
- * where we set K as a hyperParameter, and we only do softmax on those max-K data
+ * the definition of SparseMax can be referred to https://arxiv.org/pdf/1602.02068.pdf.
+ * {@code SparseMax} is a simpler implementation of sparseMax function,
+ * where we set K as a hyperParameter(default 3). We only do softmax on those max-K data,
  * and we set all the other value as 0.
  */
 public class SparseMax extends AbstractBlock{
     private static final Byte VERSION = 1;
 
     private int axis;
-    private int K;
+    private int topK;
     private NDManager manager;
 
     public SparseMax(){
         this(-1,3);
     }
 
+    public SparseMax(int axis){
+        this(axis,3);
+    }
+
     public SparseMax(int axis,int K){
         super(VERSION);
         this.axis = axis;
-        this.K = K;
+        this.topK = K;
     }
 
     @Override
@@ -40,26 +47,30 @@ public class SparseMax extends AbstractBlock{
 
     @Override
     protected NDList forwardInternal(ParameterStore parameterStore, NDList inputs, boolean training, PairList<String, Object> params) {
-        System.out.println("In sparseMax forwardInternal");
+        /*
+        A simple implementation of sparseMax, where we only calculate softMax with largest K data
+         */
         manager = inputs.getManager();
         NDArray input = inputs.singletonOrThrow();
-        if(this.axis!=-1){
-            input = input.swapAxes(this.axis,-1);   //swap the axes and get what we want to the last
+        if(axis!=-1){
+            input = input.swapAxes(axis,-1);
         }
 
-        //have a problem here with argSort
-        System.out.println("input = "+input);
-        NDArray mask = input.argSort(1,true);
-        System.out.println("mask = "+mask);
+        //level should be: the max i-th is index j in input
+        NDArray level = input.argSort(-1,false).toType(DataType.INT64,false);
+        int lastDimSize = (int)input.size(input.getShape().dimension()-1);
 
-        mask = NDArrays.where(mask.lt(K),manager.ones(mask.getShape()),manager.zeros(mask.getShape()));
-        System.out.println("mask = "+mask);
+        //maskTopK should be: the topK in input is 1 and other is zero
+        NDArray maskTopK = NDArrays.add(IntStream.range(0,topK).mapToObj(
+                j-> level.get("..., {}",j).oneHot(lastDimSize)
+        ).toArray(NDArray[]::new));
 
-        NDArray sum = mask.mul(input.exp()).sum(new int[]{-1},true).broadcast(mask.getShape());
-        System.out.println("sum = "+sum);
+        NDArray expSum = input.exp().mul(maskTopK).sum(new int[]{-1},true).broadcast(input.getShape());
+        NDArray output = input.exp().mul(maskTopK).div(expSum);
 
-        NDArray result = mask.mul(input.exp().div(sum));
-        System.out.println("result = "+result);
-        return new NDList(result);
+        if(axis!=-1) {
+            output = output.swapAxes(axis, -1);
+        }
+        return new NDList(output);
     }
 }
