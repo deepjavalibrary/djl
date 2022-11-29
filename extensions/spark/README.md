5b2e760a6489de9980f1544a5d3982fa0cadcf08
Xin Yang <105740670+xyang16@users.noreply.github.com>
2022-11-29 08:20:52 -0800

    Add Spark extension (#2162)
    * Add Spark extension
    
    * Fixes dependency
    
    * Changes
    
    Co-authored-by: Frank Liu <frankfliu2000@gmail.com>

---
+++ extensions/spark/README.md
@@ -0,0 +1,25 @@
+# Spark Support for DJL
+
+This module contains the Spark support extension with [Apache Spark](https://spark.apache.org/).
+
+## Documentation
+
+The latest javadocs can be found on [here](https://javadoc.io/doc/ai.djl.spark/spark/latest/index.html).
+
+You can also build the latest javadocs locally using the following command:
+
+```sh
+./gradlew javadoc
+```
+
+## Installation
+
+You can pull the module from the central Maven repository by including the following dependency in your `pom.xml` file:
+
+```xml
+<dependency>
+    <groupId>ai.djl.spark</groupId>
+    <artifactId>spark</artifactId>
+    <version>0.20.0</version>
+</dependency>
+```
---
+++ extensions/spark/src/main/scala/ai/djl/spark/translator/SparkImageClassificationTranslator.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package ai.djl.spark.translator
+
+import ai.djl.modality.Classifications
+import ai.djl.modality.cv.transform.{Resize, ToTensor}
+import ai.djl.ndarray.NDList
+import ai.djl.ndarray.types.{DataType, Shape}
+import ai.djl.translate.{Batchifier, Pipeline, Translator, TranslatorContext}
+import ai.djl.util.Utils
+import org.apache.spark.ml.image.ImageSchema
+import org.apache.spark.sql.Row
+
+import java.util
+
+/** A [[ai.djl.translate.Translator]] for Spark Image Classification tasks. */
+@SerialVersionUID(1L)
+class SparkImageClassificationTranslator extends Translator[Row, Classifications] with Serializable {
+
+  private var classes: util.List[String] = new util.ArrayList[String]()
+  @transient private lazy val pipeline = new Pipeline()
+    .add(new Resize(224, 224))
+    .add(new ToTensor())
+
+  /** @inheritdoc */
+  override def prepare(ctx: TranslatorContext): Unit = {
+    classes = Utils.readLines(ctx.getModel.getArtifact("synset.txt").openStream())
+  }
+
+  /** @inheritdoc */
+  override def processInput(ctx: TranslatorContext, row: Row): NDList = {
+    val height = ImageSchema.getHeight(row)
+    val width = ImageSchema.getWidth(row)
+    val channel = ImageSchema.getNChannels(row)
+    var image = ctx.getNDManager.create(ImageSchema.getData(row), new Shape(height, width, channel)).toType(DataType.UINT8, true)
+    // BGR to RGB
+    image = image.flip(2)
+    pipeline.transform(new NDList(image))
+  }
+
+  /** @inheritdoc */
+  override def processOutput(ctx: TranslatorContext, list: NDList): Classifications = {
+    var probabilitiesNd = list.singletonOrThrow
+    probabilitiesNd = probabilitiesNd.softmax(0)
+    new Classifications(classes, probabilitiesNd)
+  }
+
+  /** @inheritdoc */
+  override def getBatchifier: Batchifier = Batchifier.STACK
+}
