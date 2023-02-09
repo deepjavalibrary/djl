ddfcbc23c65255e5a4c463cf5a373bf975551f77
Zach Kimberg <kimbergz@amazon.com>
2023-02-09 12:40:39 -0800

    Remove change to testTrainAirfoilWithTabNet

--- examples/src/test/java/ai/djl/examples/training/TrainAirfoilWithTabNetTest.java
+++ examples/src/test/java/ai/djl/examples/training/TrainAirfoilWithTabNetTest.java
@@ -25,7 +25,6 @@ public class TrainAirfoilWithTabNetTest {
 
     @Test
     public void testTrainAirfoilWithTabNet() throws TranslateException, IOException {
-        TestRequirements.nightly();
         TestRequirements.engine("MXNet", "PyTorch");
         String[] args = {"-g", "1", "-e", "20", "-b", "32"};
         if (!Boolean.getBoolean("nightly")) {
