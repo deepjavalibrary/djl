3a4e3ac14517f0321847a6ff55769fb62efd5761
Xin Yang <105740670+xyang16@users.noreply.github.com>
2023-02-22 10:12:03 -0800

    [spark] Add pyspark Text predictors (#2414)

--- extensions/spark/setup/djl_spark/task/text/__init__.py
+++ extensions/spark/setup/djl_spark/task/text/__init__.py
@@ -10,3 +10,21 @@
 # or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS"
 # BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied. See the License for
 # the specific language governing permissions and limitations under the License.
+
+"""DJL Spark Tasks Text API."""
+
+from . import huggingface_text_decoder
+from . import huggingface_text_encoder
+from . import huggingface_text_tokenizer
+from . import text_embedder
+
+HuggingFaceTextDecoder = huggingface_text_decoder.HuggingFaceTextDecoder
+HuggingFaceTextEncoder = huggingface_text_encoder.HuggingFaceTextEncoder
+HuggingFaceTextTokenizer = huggingface_text_tokenizer.HuggingFaceTextTokenizer
+TextEmbedder = text_embedder.TextEmbedder
+
+# Remove unnecessary modules to avoid duplication in API.
+del huggingface_text_decoder
+del huggingface_text_encoder
+del huggingface_text_tokenizer
+del text_embedder
