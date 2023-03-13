7b49aa20cb83071527ed6698087bcdb279904e85
Frank Liu <frankfliu2000@gmail.com>
2023-03-13 12:55:16 -0700

    [api] Adds chunked output support (#2453)
    * [api] Adds chunked output support
    
    * Simplify chunked BytesSupplier

---
+++ api/src/main/java/ai/djl/modality/ChunkedBytesSupplier.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package ai.djl.modality;
+
+import ai.djl.ndarray.BytesSupplier;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/** A {link BytesSupplier} that supports chunked reading. */
+public class ChunkedBytesSupplier implements BytesSupplier {
+
+    private LinkedBlockingQueue<BytesSupplier> queue;
+    private AtomicBoolean completed;
+
+    /** Constructs a new {code ChunkedBytesSupplier} instance. */
+    public ChunkedBytesSupplier() {
+        queue = new LinkedBlockingQueue<>();
+        completed = new AtomicBoolean();
+    }
+
+    /**
+     * Appends content to the {@code BytesSupplier}.
+     *
+     * @param data bytes to append
+     * @param lastChunk true if this is the last chunk
+     */
+    public void appendContent(byte[] data, boolean lastChunk) {
+        queue.offer(BytesSupplier.wrap(data));
+        if (lastChunk) {
+            completed.set(true);
+        }
+    }
+
+    /**
+     * Returns {@code true} if has more chunk.
+     *
+     * @return {@code true} if has more chunk
+     */
+    public boolean hasNext() {
+        return !completed.get() || !queue.isEmpty();
+    }
+
+    /**
+     * Returns the next chunk.
+     *
+     * @param timeout the maximum time to wait
+     * @param unit the time unit of the timeout argument
+     * @return the next chunk
+     * @throws InterruptedException if the thread is interrupted
+     */
+    public byte[] nextChunk(long timeout, TimeUnit unit) throws InterruptedException {
+        BytesSupplier data = queue.poll(timeout, unit);
+        if (data == null) {
+            throw new IllegalStateException("Read chunk timeout.");
+        }
+        return data.getAsBytes();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public byte[] getAsBytes() {
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+            while (hasNext()) {
+                bos.write(nextChunk(1, TimeUnit.MINUTES));
+            }
+            return bos.toByteArray();
+        } catch (IOException | InterruptedException e) {
+            throw new AssertionError("Failed to read BytesSupplier", e);
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public ByteBuffer toByteBuffer() {
+        return ByteBuffer.wrap(getAsBytes());
+    }
+}
---
+++ api/src/test/java/ai/djl/modality/ChunkedBytesSupplierTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package ai.djl.modality;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.concurrent.TimeUnit;
+
+public class ChunkedBytesSupplierTest {
+
+    @Test
+    public void test() throws InterruptedException, IOException {
+        ChunkedBytesSupplier supplier = new ChunkedBytesSupplier();
+        Assert.assertThrows(() -> supplier.nextChunk(1, TimeUnit.MICROSECONDS));
+
+        supplier.appendContent(new byte[] {1, 2}, false);
+        supplier.appendContent(new byte[] {3}, true);
+
+        ByteBuffer bb = supplier.toByteBuffer();
+        Assert.assertEquals(bb.array(), new byte[] {1, 2, 3});
+
+        ChunkedBytesSupplier data = new ChunkedBytesSupplier();
+        data.appendContent(new byte[0], false);
+        data.appendContent(new byte[] {1, 2}, true);
+
+        Assert.assertTrue(data.hasNext());
+        Assert.assertEquals(data.nextChunk(1, TimeUnit.MILLISECONDS).length, 0);
+        Assert.assertEquals(data.nextChunk(1, TimeUnit.MILLISECONDS), new byte[] {1, 2});
+
+        Assert.assertFalse(data.hasNext());
+    }
+}
