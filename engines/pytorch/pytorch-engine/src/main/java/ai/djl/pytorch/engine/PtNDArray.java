e334a3b7a25178cf4fcd62fb12c89c9f2361cb7c
KexinFeng <fenkexin@amazon.com>
2023-07-02 11:27:45 -0700

    fix

--- engines/pytorch/pytorch-engine/src/main/java/ai/djl/pytorch/engine/PtNDArray.java
+++ engines/pytorch/pytorch-engine/src/main/java/ai/djl/pytorch/engine/PtNDArray.java
@@ -1351,7 +1351,7 @@ public class PtNDArray extends NativeResource<Long> implements NDArray {
         int otherDim = other.getShape().dimension();
         if (selfDim != otherDim || selfDim > 2) {
             throw new UnsupportedOperationException(
-                    "Dimension mismatch or high dimensional dot operation is not supported. Please"
+                    "Dimension mismatch or dimension is greater than 2.  Dot product is only applied on two 1D vectors. For high dimensions, please"
                             + " use .matMul instead.");
         }
         return JniUtils.dot(this, manager.from(other));
