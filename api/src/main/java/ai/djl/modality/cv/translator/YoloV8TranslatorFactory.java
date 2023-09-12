94c6564bed258e352e9183d75d4f22da1c28e637
KexinFeng <fenkexin@amazon.com>
2023-09-12 10:31:49 -0700

    format

--- api/src/main/java/ai/djl/modality/cv/translator/YoloV8TranslatorFactory.java
+++ api/src/main/java/ai/djl/modality/cv/translator/YoloV8TranslatorFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
  * with the License. A copy of the License is located at
@@ -12,22 +12,24 @@
  */
 package ai.djl.modality.cv.translator;
 
-import java.io.Serializable;
-import java.util.Map;
-
 import ai.djl.Model;
 import ai.djl.modality.cv.Image;
 import ai.djl.modality.cv.output.DetectedObjects;
 import ai.djl.translate.Translator;
 
-/** An {@link TranslatorFactory} that creates a {@link YoloV8Translator} instance. */
-public class YoloV8TranslatorFactory extends ObjectDetectionTranslatorFactory implements Serializable {
+import java.io.Serializable;
+import java.util.Map;
 
-  private static final long serialVersionUID = 1L;
-  
-  /** {@inheritDoc} */
-  @Override
-  protected Translator<Image, DetectedObjects> buildBaseTranslator(Model model, Map<String, ?> arguments) {
-    return YoloV8Translator.builder(arguments).build();
-  }
+/** A translatorFactory that creates a {@link YoloV8Translator} instance. */
+public class YoloV8TranslatorFactory extends ObjectDetectionTranslatorFactory
+        implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /** {@inheritDoc} */
+    @Override
+    protected Translator<Image, DetectedObjects> buildBaseTranslator(
+            Model model, Map<String, ?> arguments) {
+        return YoloV8Translator.builder(arguments).build();
+    }
 }
