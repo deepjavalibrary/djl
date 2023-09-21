9dc6887b0bfe7b45d5f2a59fa937fba5634492a3
Frank Liu <frankfliu2000@gmail.com>
2023-09-21 16:24:26 -0500

    [onnxruntime] Upgrades OnnxRuntime to 1.16.0 (#2784)

--- engines/onnxruntime/onnxruntime-engine/src/main/java/ai/djl/onnxruntime/engine/OrtEngine.java
+++ engines/onnxruntime/onnxruntime-engine/src/main/java/ai/djl/onnxruntime/engine/OrtEngine.java
@@ -97,7 +97,7 @@ public final class OrtEngine extends Engine {
     /** {@inheritDoc} */
     @Override
     public String getVersion() {
-        return "1.15.1";
+        return "1.16.0";
     }
 
     /** {@inheritDoc} */
