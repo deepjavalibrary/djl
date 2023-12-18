0b9e1e1ecfdc86cea6a7222ae2c2140e8faa994d
Frank Liu <frankfliu2000@gmail.com>
2023-12-18 09:40:43 -0800

    [api] Handles both JNA conflict and missing case

--- api/src/main/java/ai/djl/util/cuda/CudaUtils.java
+++ api/src/main/java/ai/djl/util/cuda/CudaUtils.java
@@ -262,16 +262,14 @@ public final class CudaUtils {
         } catch (UnsatisfiedLinkError e) {
             logger.debug("cudart library not found.");
             logger.trace("", e);
-            return null;
-        } catch (IncompatibleClassChangeError e) {
+        } catch (LinkageError e) {
             logger.warn("You have a conflict version of JNA in the classpath.");
             logger.debug("", e);
-            return null;
         } catch (SecurityException e) {
             logger.warn("Access denied during loading cudart library.");
             logger.trace("", e);
-            return null;
         }
+        return null;
     }
 
     private static String[] execute(int deviceId) {
