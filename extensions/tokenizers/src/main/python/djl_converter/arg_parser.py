d7ce12db6f0f40f555d004b1895bd316ebcfd325
Frank Liu <frankfliu2000@gmail.com>
2024-05-13 16:20:19 -0700

    [huggingface] Adds more option to convert onnx model (#3180)

--- extensions/tokenizers/src/main/python/djl_converter/arg_parser.py
+++ extensions/tokenizers/src/main/python/djl_converter/arg_parser.py
@@ -17,6 +17,50 @@ import os
 
 def converter_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output-dir", help="Model output directory")
+    parser.add_argument("-f",
+                        "--output-format",
+                        default="PyTorch",
+                        choices=["PyTorch", "OnnxRuntime", "Rust"],
+                        help="Model output format")
+    parser.add_argument("-u",
+                        "--cpu-only",
+                        action='store_true',
+                        help="Only validate jit traced model on CPU")
+    parser.add_argument("-m",
+                        "--model-id",
+                        help="Huggingface model_id to convert")
+    parser.add_argument("-r", "--revision", help="Huggingface model revision")
+    parser.add_argument("-t", "--token", help="Huggingface token")
+    parser.add_argument("--optimize",
+                        choices=["O1", "O2", "O3", "O4"],
+                        help="Optimization option for ONNX models")
+    parser.add_argument(
+        "--device",
+        help='The device to use to do the export. Defaults to "cpu".')
+    parser.add_argument(
+        "--dtype",
+        choices=["fp32", "fp16", "bf16"],
+        help="The floating point precision to use for the export.")
+    parser.add_argument(
+        "--trust-remote-code",
+        action="store_true",
+        help=
+        "Allows to use custom code for the modeling hosted in the model repository. This option should only be set for repositories you trust and in which you have read the code, as it will execute on your local machine arbitrary code present in the model repository."
+    )
+
+    args = parser.parse_args()
+    if args.output_dir is None:
+        args.output_dir = "."
+
+    if not os.path.exists(args.output_dir):
+        raise ValueError(f"Invalid output directory: {args.output_dir}.")
+
+    return args
+
+
+def importer_args():
+    parser = argparse.ArgumentParser()
     parser.add_argument("-l",
                         "--limit",
                         type=int,
