3498b857bf0d87da154fb82d34b520f7f675d7d5
Frank Liu <frankfliu2000@gmail.com>
2024-05-17 14:50:36 -0700

    Fixes checkstyle issues

--- extensions/tokenizers/src/test/java/ai/djl/huggingface/zoo/package-info.java
+++ extensions/tokenizers/src/test/java/ai/djl/huggingface/zoo/package-info.java
@@ -10,4 +10,6 @@
  * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
  * and limitations under the License.
  */
+
+/** Contains tests for the huggingface zoo module. */
 package ai.djl.huggingface.zoo;
