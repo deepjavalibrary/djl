e4dc650a51b25b1931706bfc76993796906faf23
Giuseppe Barbieri <elect86@gmail.com>
2024-05-27 23:29:05 +0200

    testing

--- buildSrc/src/main/kotlin/ai/djl/stats.gradle.kts
+++ buildSrc/src/main/kotlin/ai/djl/stats.gradle.kts
@@ -1,32 +1,42 @@
+@file:OptIn(ExperimentalTime::class)
+
 package ai.djl
 
+import org.gradle.kotlin.dsl.support.serviceOf
 import java.util.*
 import kotlin.time.Duration
-import kotlin.time.Duration.Companion.seconds
+import kotlin.time.ExperimentalTime
+import kotlin.time.TimeSource
 
 plugins {
     `java-library`
 }
 
+val timeSource = TimeSource.Monotonic
 val testsResults = TreeMap<Duration, String>(Comparator.reverseOrder())
 
 tasks.test {
-
-    addTestListener(object : TestListener {
-        override fun beforeSuite(suite: TestDescriptor) {}
-        override fun afterSuite(suite: TestDescriptor, result: TestResult) {}
-        override fun beforeTest(testDescriptor: TestDescriptor) {}
-        override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {
-            val duration = (result.endTime - result.startTime).seconds
-            testsResults[duration] = testDescriptor.className + '.' + testDescriptor.name
-        }
-    })
-
+    doFirst { startTime = timeSource.markNow() }
     doLast {
+        if (state.didWork)
+            testsResults[startTime - timeSource.markNow()] = project.name
+    }
+}
+
+class DoSomething : FlowAction<FlowParameters.None> {
+    override fun execute(parameters: FlowParameters.None) {
         if ("build" in gradle.startParameter.taskNames && testsResults.isNotEmpty()) {
             println("========== Test duration ==========")
-            for((value, key) in testsResults.entries.take(5))
+            for ((value, key) in testsResults.entries.take(5))
                 println("\t$value:\t${key}s")
         }
     }
 }
+
+gradle.serviceOf<FlowScope>().always(DoSomething::class.java) { }
+
+var Task.startTime: TimeSource.Monotonic.ValueTimeMark
+    get() = extensions.getByName<TimeSource.Monotonic.ValueTimeMark>("starTime")
+    set(value) {
+        extensions.add("startTime", timeSource.markNow())
+    }
