94f4e7d7064dda8628761f70741da55055754933
Giuseppe Barbieri <elect86@gmail.com>
2024-05-28 00:07:13 +0200

    testing

--- buildSrc/src/main/kotlin/ai/djl/stats.gradle.kts
+++ buildSrc/src/main/kotlin/ai/djl/stats.gradle.kts
@@ -1,8 +1,6 @@
-@file:OptIn(ExperimentalTime::class)
-
 package ai.djl
 
-import org.gradle.kotlin.dsl.support.serviceOf
+import org.gradle.tooling.events.OperationCompletionListener
 import java.util.*
 import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
@@ -12,31 +10,50 @@ plugins {
     `java-library`
 }
 
+@ExperimentalTime
 val timeSource = TimeSource.Monotonic
 val testsResults = TreeMap<Duration, String>(Comparator.reverseOrder())
 
 tasks.test {
-    doFirst { startTime = timeSource.markNow() }
+    doFirst {
+        @OptIn(ExperimentalTime::class)
+        startTime = timeSource.markNow()
+    }
     doLast {
+        @OptIn(ExperimentalTime::class)
         if (state.didWork)
             testsResults[startTime - timeSource.markNow()] = project.name
     }
 }
 
-class DoSomething : FlowAction<FlowParameters.None> {
-    override fun execute(parameters: FlowParameters.None) {
-        if ("build" in gradle.startParameter.taskNames && testsResults.isNotEmpty()) {
+
+abstract class StatisticsService : BuildService<StatisticsService.Parameters>,
+                                   OperationCompletionListener, AutoCloseable {
+
+    interface Parameters : BuildServiceParameters {
+        var testsResults: TreeMap<Duration, String>
+    }
+
+    override fun close() {
+        if ("build" in gradle.startParameter.taskNames && parameters.testsResults.isNotEmpty()) {
             println("========== Test duration ==========")
-            for ((value, key) in testsResults.entries.take(5))
+            for ((value, key) in parameters.testsResults.entries.take(5))
                 println("\t$value:\t${key}s")
         }
     }
 }
 
-gradle.serviceOf<FlowScope>().always(DoSomething::class.java) { }
+//gradle.buildFinished {
+//    if ("build" in gradle!!.startParameter.taskNames && testsResults.isNotEmpty()) {
+//        println("========== Test duration ==========")
+//        for ((value, key) in testsResults.entries.take(5))
+//            println("\t$value:\t${key}s")
+//    }
+//}
 
+@ExperimentalTime
 var Task.startTime: TimeSource.Monotonic.ValueTimeMark
     get() = extensions.getByName<TimeSource.Monotonic.ValueTimeMark>("starTime")
     set(value) {
-        extensions.add("startTime", timeSource.markNow())
+        extensions.add("startTime", value)
     }
