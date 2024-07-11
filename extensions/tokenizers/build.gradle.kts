b171572b0db68ae5055374b23d537596552751bb
Frank Liu <frankfliu2000@gmail.com>
2024-07-11 15:28:46 -0700

    [tokenizers] Fixes huggingface build for Windows (#3330)

--- extensions/tokenizers/build.gradle.kts
+++ extensions/tokenizers/build.gradle.kts
@@ -25,9 +25,13 @@ tasks {
     compileJava { dependsOn(processResources) }
 
     processResources {
-        inputs.properties(mapOf("djlVersion" to libs.versions.djl.get(),
-            "tokenizersVersion" to libs.versions.tokenizers.get(),
-            "version" to version))
+        inputs.properties(
+            mapOf(
+                "djlVersion" to libs.versions.djl.get(),
+                "tokenizersVersion" to libs.versions.tokenizers.get(),
+                "version" to version
+            )
+        )
         val baseResourcePath = "${project.projectDir}/build/resources/main"
         outputs.dirs(File("${baseResourcePath}/native/lib"), File("${baseResourcePath}/nlp"))
 
@@ -35,9 +39,9 @@ tasks {
             var url = "https://publish.djl.ai/tokenizers"
             val (tokenizers, djl) = libs.versions.tokenizers.get() to libs.versions.djl.get()
             val files = mapOf(
-                "win-x86_64/libwinpthread-1.dll" to "extra",
-                "win-x86_64/libgcc_s_seh-1.dll" to "extra",
-                "win-x86_64/libstdc%2B%2B-6.dll" to "extra",
+                "win-x86_64/cpu/libwinpthread-1.dll" to "extra/win-x86_64/libwinpthread-1.dll",
+                "win-x86_64/cpu/libgcc_s_seh-1.dll" to "extra/win-x86_64/libgcc_s_seh-1.dll",
+                "win-x86_64/cpu/libstdc%2B%2B-6.dll" to "extra/win-x86_64/libstdc%2B%2B-6.dll",
                 "win-x86_64/cpu/tokenizers.dll" to "$tokenizers/jnilib/$djl",
                 "linux-x86_64/cpu/libtokenizers.so" to "$tokenizers/jnilib/$djl",
                 "linux-aarch64/cpu/libtokenizers.so" to "$tokenizers/jnilib/$djl",
@@ -49,7 +53,12 @@ tasks {
                 val file = jnilibDir / URLDecoder.decode(key, "UTF-8")
                 if (file.exists())
                     project.logger.lifecycle("prebuilt or cached file found for $key")
-                else if ("extra" == value || !project.hasProperty("jni")) {
+                else if (value.startsWith("extra")) {
+                    project.logger.lifecycle("Downloading $url/$value")
+                    file.parentFile.mkdirs()
+                    val downloadPath = "$url/$value".url
+                    downloadPath into file
+                } else if (!project.hasProperty("jni")) {
                     project.logger.lifecycle("Downloading $url/$value/$key")
                     file.parentFile.mkdirs()
                     val downloadPath = "$url/$value/$key".url
