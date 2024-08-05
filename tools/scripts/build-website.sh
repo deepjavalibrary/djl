e03f0ec043399353140407f5ffd15fbce8226656
Varun Dutta <72882713+Varun-Dutta@users.noreply.github.com>
2024-08-05 11:48:09 -0700

    [docs] update docs.yml and docs workflow to support versioning (#3365)

---
+++ tools/scripts/build-website.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -ex
+
+VERSION_NUMBER=$1
+if [[ "$VERSION_NUMBER" == "" ]]; then
+  VERSION_NUMBER=master
+fi
+
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo "Updating online runner ..."
+python3 "$BASE_DIR/add_online_runner.py"
+
+# Configure Git User, mike will create gh-pages branch
+git config user.name "nobody"
+git config user.email "nobody@localhost"
+
+echo "Building docs for $VERSION_NUMBER"
+mike deploy "$VERSION_NUMBER" -F docs/mkdocs.yml -b gh-pages
+
+git checkout gh-pages
+
+echo "generating versions.json"
+current_version=$(awk -F '=' '/djl / {gsub(/ ?"/, "", $2); print $2}' "$BASE_DIR/../../gradle/libs.versions.toml" | awk -F '.' '{print $2}')
+versions='[{"version":"master","title":"master","aliases":[]}'
+for i in {1..4}; do
+  version="0.$((current_version - i)).0"
+  versions="$versions, {\"version\":\"$version\",\"title\":\"$version\",\"aliases\":[]}"
+done
+versions="$versions]"
+echo "$versions" | jq "." > "$BASE_DIR/../../versions.json"
