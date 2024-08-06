824ef1244796d66875e564e6991ee7d25e0fa3b4
Varun Dutta <72882713+Varun-Dutta@users.noreply.github.com>
2024-08-06 01:38:08 -0700

    Update build-website.sh

--- tools/scripts/build-website.sh
+++ tools/scripts/build-website.sh
@@ -2,6 +2,17 @@
 
 set -ex
 
+echo "generating versions.json"
+pwd
+current_version=$(awk -F '=' '/djl / {gsub(/ ?"/, "", $2); print $2}' "/home/runner/work/djl/djl/gradle/libs.versions.toml" | awk -F '.' '{print $2}')
+versions='[{"version":"master","title":"master","aliases":[]}'
+for i in {1..4}; do
+  version="0.$((current_version - i)).0"
+  versions="$versions, {\"version\":\"$version\",\"title\":\"$version\",\"aliases\":[]}"
+done
+versions="$versions]"
+echo "$versions" | jq "." > "./versions.json"
+
 VERSION_NUMBER=$1
 if [[ "$VERSION_NUMBER" == "" ]]; then
   VERSION_NUMBER=master
