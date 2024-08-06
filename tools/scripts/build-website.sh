d3c0a41d40696389cba05ac6edb84fda1ea64f0a
Frank Liu <frankfliu2000@gmail.com>
2024-08-06 09:21:37 -0700

    refactor build-website.sh

--- tools/scripts/build-website.sh
+++ tools/scripts/build-website.sh
@@ -2,7 +2,9 @@
 
 set -ex
 
-echo "generating versions.json" 
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo "generating versions.json"
 current_version=$(awk -F '=' '/djl / {gsub(/ ?"/, "", $2); print $2}' "gradle/libs.versions.toml" | awk -F '.' '{print $2}')
 versions='[{"version":"master","title":"master","aliases":[]}'
 for i in {1..4}; do
@@ -10,21 +12,28 @@ for i in {1..4}; do
   versions="$versions, {\"version\":\"v$version\",\"title\":\"v$version\",\"aliases\":[]}"
 done
 versions="$versions]"
-echo "$versions" | jq "." > "./versions.json"
+echo "$versions" | jq "." > "$BASE_DIR/../../versions.json"
 
 VERSION_NUMBER=$1
 if [[ "$VERSION_NUMBER" == "" ]]; then
   VERSION_NUMBER=master
 fi
 
-BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 echo "Updating online runner ..."
 python3 "$BASE_DIR/add_online_runner.py"
 
 # Configure Git User, mike will create gh-pages branch
-git config user.name "nobody"
-git config user.email "nobody@localhost"
+if [[ -z "$(git config user.name)" ]]; then
+  git config user.name "nobody"
+fi
+if [[ -z "$(git config user.email)" ]]; then
+  git config user.email "nobody@localhost"
+fi
 
 echo "Building docs for $VERSION_NUMBER"
 mike deploy "$VERSION_NUMBER" -F docs/mkdocs.yml -b gh-pages
+
+git stash
+git checkout gh-pages
+git stash pop
+
