7cad1d621c973a1c83098ab4bfeba6de085377c8
Varun Dutta <72882713+Varun-Dutta@users.noreply.github.com>
2024-08-06 01:36:02 -0700

    Update build-website.sh

--- tools/scripts/build-website.sh
+++ tools/scripts/build-website.sh
@@ -18,16 +18,3 @@ git config user.email "nobody@localhost"
 
 echo "Building docs for $VERSION_NUMBER"
 mike deploy "$VERSION_NUMBER" -F docs/mkdocs.yml -b gh-pages
-
-git checkout gh-pages
-
-echo "generating versions.json"
-pwd
-current_version=$(awk -F '=' '/djl / {gsub(/ ?"/, "", $2); print $2}' "/home/runner/work/djl/djl/gradle/libs.versions.toml" | awk -F '.' '{print $2}')
-versions='[{"version":"master","title":"master","aliases":[]}'
-for i in {1..4}; do
-  version="0.$((current_version - i)).0"
-  versions="$versions, {\"version\":\"$version\",\"title\":\"$version\",\"aliases\":[]}"
-done
-versions="$versions]"
-echo "$versions" | jq "." > "./versions.json"
