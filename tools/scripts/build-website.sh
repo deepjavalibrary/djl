135fc563526170b9dce6d582ce3564810132d739
Varun Dutta <72882713+Varun-Dutta@users.noreply.github.com>
2024-08-07 15:18:27 -0700

    [docs] Build versions.json before mike deploy (#3392)
    * Fixes generated versions.json issue
    
    ---------
    
    Co-authored-by: Frank Liu <frankfliu2000@gmail.com>

--- tools/scripts/build-website.sh
+++ tools/scripts/build-website.sh
@@ -2,31 +2,36 @@
 
 set -ex
 
+REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
+
 VERSION_NUMBER=$1
 if [[ "$VERSION_NUMBER" == "" ]]; then
   VERSION_NUMBER=master
 fi
 
-BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 echo "Updating online runner ..."
-python3 "$BASE_DIR/add_online_runner.py"
+python3 "$REPO_ROOT/tools/scripts/add_online_runner.py"
 
 # Configure Git User, mike will create gh-pages branch
-git config user.name "nobody"
-git config user.email "nobody@localhost"
+if [[ -z "$(git config user.name)" ]]; then
+  git config user.name "nobody"
+fi
+if [[ -z "$(git config user.email)" ]]; then
+  git config user.email "nobody@localhost"
+fi
+
+echo "generating versions.json"
+current_version=$(awk -F '=' '/djl / {gsub(/ ?"/, "", $2); print $2}' "gradle/libs.versions.toml" | awk -F '.' '{print $2}')
+versions='[{"version":"master","title":"master","aliases":[]}'
+for i in {1..4}; do
+  version="0.$((current_version - i)).0"
+  versions="$versions, {\"version\":\"v$version\",\"title\":\"v$version\",\"aliases\":[]}"
+done
+versions="$versions]"
 
 echo "Building docs for $VERSION_NUMBER"
 mike deploy "$VERSION_NUMBER" -F docs/mkdocs.yml -b gh-pages
 
 git checkout gh-pages
 
-echo "generating versions.json"
-current_version=$(awk -F '=' '/djl / {gsub(/ ?"/, "", $2); print $2}' "$BASE_DIR/../../gradle/libs.versions.toml" | awk -F '.' '{print $2}')
-versions='[{"version":"master","title":"master","aliases":[]}'
-for i in {1..4}; do
-  version="0.$((current_version - i)).0"
-  versions="$versions, {\"version\":\"$version\",\"title\":\"$version\",\"aliases\":[]}"
-done
-versions="$versions]"
-echo "$versions" | jq "." > "$BASE_DIR/../../versions.json"
+echo "$versions" | jq "." >"$REPO_ROOT/versions.json"
