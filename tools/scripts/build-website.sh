56459187acf2a106b0c268b1f156c31dfb33968e
Frank Liu <frankfliu2000@gmail.com>
2024-08-07 13:14:54 -0700

    Fixes generated versions.json issue

--- tools/scripts/build-website.sh
+++ tools/scripts/build-website.sh
@@ -2,17 +2,7 @@
 
 set -ex
 
-BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-echo "generating versions.json"
-current_version=$(awk -F '=' '/djl / {gsub(/ ?"/, "", $2); print $2}' "gradle/libs.versions.toml" | awk -F '.' '{print $2}')
-versions='[{"version":"master","title":"master","aliases":[]}'
-for i in {1..4}; do
-  version="0.$((current_version - i)).0"
-  versions="$versions, {\"version\":\"v$version\",\"title\":\"v$version\",\"aliases\":[]}"
-done
-versions="$versions]"
-echo "$versions" | jq "." > "$BASE_DIR/../../versions.json"
+REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 
 VERSION_NUMBER=$1
 if [[ "$VERSION_NUMBER" == "" ]]; then
@@ -20,7 +10,7 @@ if [[ "$VERSION_NUMBER" == "" ]]; then
 fi
 
 echo "Updating online runner ..."
-python3 "$BASE_DIR/add_online_runner.py"
+python3 "$REPO_ROOT/tools/scripts/add_online_runner.py"
 
 # Configure Git User, mike will create gh-pages branch
 if [[ -z "$(git config user.name)" ]]; then
@@ -30,10 +20,18 @@ if [[ -z "$(git config user.email)" ]]; then
   git config user.email "nobody@localhost"
 fi
 
+echo "generating versions.json"
+current_version=$(awk -F '=' '/djl / {gsub(/ ?"/, "", $2); print $2}' "gradle/libs.versions.toml" | awk -F '.' '{print $2}')
+versions='[{"version":"master","title":"master","aliases":[]}'
+for i in {1..4}; do
+  version="0.$((current_version - i)).0"
+  versions="$versions, {\"version\":\"v$version\",\"title\":\"v$version\",\"aliases\":[]}"
+done
+versions="$versions]"
+
 echo "Building docs for $VERSION_NUMBER"
 mike deploy "$VERSION_NUMBER" -F docs/mkdocs.yml -b gh-pages
 
-git stash
 git checkout gh-pages
-git stash pop
 
+echo "$versions" | jq "." >"$REPO_ROOT/versions.json"
