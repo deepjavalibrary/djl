ff09ba2639da15506dc77f83cbcc9ba946830d39
Xin Yang <105740670+xyang16@users.noreply.github.com>
2024-12-02 15:12:26 -0800

    [onnxruntime] Updates OnnxRuntime engine to 1.20.0 (#3549)

--- engines/onnxruntime/onnxruntime-engine/src/main/java/ai/djl/onnxruntime/engine/OrtEngine.java
+++ engines/onnxruntime/onnxruntime-engine/src/main/java/ai/djl/onnxruntime/engine/OrtEngine.java
@@ -108,7 +108,7 @@ public final class OrtEngine extends Engine {
     /** {@inheritDoc} */
     @Override
     public String getVersion() {
-        return "1.19.0";
+        return "1.20.0";
     }
 
     /** {@inheritDoc} */
